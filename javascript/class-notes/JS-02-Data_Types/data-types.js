53192b4fbe2c9a631b82d3718b06304c7e4283ef
sabanferik <sbnftmaf@gmail.com>
2023-12-21 12:45:10 +0100

    data typler ilk gün son commit

--- javascript/class-notes/JS-02-Data_Types/data-types.js
+++ javascript/class-notes/JS-02-Data_Types/data-types.js
@@ -85,7 +85,6 @@ console.log(Math.max(34,1,45,3,53,15,13))
 console.log(Math.min(34,1,45,3,53,15,13))
 
 /* -------------------------------------------------------------------------- */
-console.clear()
 
 // RANDOM: rastgele
 // rastgele sayı üretmek için kullanılır
@@ -125,8 +124,135 @@ console.log(Math.sqrt(56).toFixed(2))
 let küpkok=125**(1/3)
 console.log("küpkök",küpkok)
 console.log(Math.cbrt(125))
+console.clear()
 
 /* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+//!                              STRING DATA TYPE                             */
+/* -------------------------------------------------------------------------- */
+// tırnaklar içinde ifade edilen değerler stringdir.
+// stringler harf,işaretler,sayılar
+let ad="Javascript"
+console.log(ad)
+
+console.log(ad[0])
+console.log(ad[3])
+console.log(ad[7],ad[0],ad[1])
+
+//immutable
+ad[7]="u"
+console.log(ad)
+
+ad="Node.js"
+console.log(ad)
+
+/* -------------------------------------------------------------------------- */
+// Escape characters- Kaçış karakterleri
+// \ kaçış karakterleri \ile tanımlanır
+// \n yeni bir satıra geçmeyi sağlar
+// \t tab işlemi yapar(8 karakter boşluk bırakır)
+
+let deger='Merhaba Ayşe\'nin ödevi'
+console.log(deger)
+let siir="Merhaba yenigün\nNeden sisli bu hava"
+console.log(siir)
+
+let developer="Frontend\tDeveloper"
+console.log(developer)
+/* -------------------------------------------------------------------------- */
+//! String birleştirme
+
+// 1.concat metodu
+
+let adi="Ali"
+let soyad="Güçlü"
+
+// string1.concat(string2)
+console.log(adi.concat(soyad))
+/* -------------------------------------------------------------------------- */
+// 2.  + işareti ile concatanation
+
+console.log(adi+soyad)
+console.log("Adınız Soyadınız:"+adi+soyad)
+
+//!Template literal yöntemi :=> ES6ile gelen özelliklerden
+
+// stringleri dinamik olarak kullanmayı sağlar
+// `` 
+// stringler yazılırken değişkenlerde kullanabilmemizi sağlar
+
+adi="Ali"
+soyad="Seven"
+let job="Fullstack developer"
+
+// 2. yöntem ile 
+console.log("Personelimiz "+adi+" "+soyad+" kurumumuzda " +job+" olarak çalışmaktadır" )
+// template literal yöntemi ile 
+
+console.log(`Personelimiz ${adi} ${soyad} 
+
+kurumumuzda ${job} 
+
+olarak çalışmaktadır`)
+/* -------------------------------------------------------------------------- */
+console.clear()
+
+// BOOLEAN DATA TYPE
+// true - false değeri tutar
+
+let isOpen=true
+console.log(typeof isOpen)
+
+let isSmall= 4>15
+console.log(isSmall)
+
+
+sifre="3de4"
+
+let kontrol=(sifre==="3de")
+console.log(kontrol)
+console.log(typeof kontrol)
+
+/* -------------------------------------------------------------------------- */
+// UNDEFINED DATA TYPE
+// bir değişkene bir değer atamadığınız zaman undefined
+// bir fonksiyon geri değer döndürmüyorsa undefined olur
+
+// undefined:tanımsız
+
+let val;
+console.log(val)
+
+val=345
+console.log(val)
+
+function example(){
+    let toplam=45+13
+    // return toplam
+}
+console.log(example())
+
+/* -------------------------------------------------------------------------- */
+// NULL DATA TYPE
+
+let empty=null
+console.log(empty)
+
+// js bug
+console.log(typeof empty)
+
+/* -------------------------------------------------------------------------- */
+// NONPRIMITIVE DATA TYPE - referans tipli değişkenler
+//array
+let liste=["kalem","silgi","defter"]
+console.log(liste)
+
+//Object
+let nesne={ad:"Ali",soyad:"Güven",yas:34}
+console.log(nesne)
+
+
+
 
 
 
