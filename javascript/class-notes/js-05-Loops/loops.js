63365268cced5d805a31e7b64938e04d9f965b65
sabanferik <sbnftmaf@gmail.com>
2024-01-09 10:17:24 +0100

    JS Loops lar ilk ders sonu

---
+++ javascript/class-notes/js-05-Loops/loops.js
@@ -0,0 +1,56 @@
+
+// * =======================================================
+// *                     DÖNGÜLER
+// * =======================================================
+
+//************************ FOR ****************************
+
+
+//? örnek ekrana 10 kere merhaba yazdiran kodu yaziniz
+
+//? ornek ekrana 10 kere merhaba yazdırın
+
+// for (let index = 1; index <= 10; index++) {
+//     console.log(index, "MERHABA");
+//   }
+
+  //? 1 den Ne kadar olan sayilarin toplamini ekrana yazdiriniz
+
+//   const N=+prompt("lütfen N i giriniz")
+
+// let toplam=0
+
+// for (let index = 1; index <= N; index++) {
+  
+//     toplam=toplam +index
+// }
+
+//   console.log(toplam);
+
+
+  //? ÖRNEK: Dışarıdan girilen sayının Asal olup olmadığını tespit ederek sonucu yazdıran programı yazınız.
+
+//   const sayi= +prompt("asallik kontrolü icin bir sayi giriniz")
+//     let asal=true
+//   for (let i=2; i< sayi; i++){
+//     if (sayi%i==0){
+//       asal=false
+//     }
+//   }
+
+//   console.log(asal ? "asaldir" : "asal degildir");
+
+  // //? Ornek: 0-100 Arasinda 7 adet rasgele tamsayi sayi ureten kodu
+// //? for dongusu ile yaziniz..
+
+// const rast=Math.random(*100);
+// const rast2=Mat.ceil(rast);
+// console.log(rast,rast2);
+
+
+//* NOT:
+//* Math.floor() =>en yakin alt tamsayiya yuvarlar
+//* Math.ceil() =>en yaki ust tamsayiya yuvarlar
+//* Math.round() => degerine gore nereye yakınsa oraya yuvarlar.
+//* Math.trunc() => kesirli sayinin tam kismini alir.
+
