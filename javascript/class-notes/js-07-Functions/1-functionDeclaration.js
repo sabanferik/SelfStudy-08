9647cdb0b349227972738dae47baf7190382e2f8
sabanferik <sbnftmaf@gmail.com>
2024-01-11 09:27:37 +0100

    Fuction Declaration ilk

---
+++ javascript/class-notes/js-07-Functions/1-functionDeclaration.js
@@ -0,0 +1,24 @@
+// ? ==========================================================
+// ?                    FONKSİYONLAR
+// ? ==========================================================
+
+// !-----------------------------------------------------------
+// ! 1.YÖNTEM  : FUNCTION DECLARATION
+// !-----------------------------------------------------------
+
+//? Bir fonksiyon belirli bir görevi gerçekleştirmek için tasarlanmış,
+//? yeniden kullanılabilir, (tek bir görev için tek fonksiyon olması daha iyidir)
+//? test etmesi kolaydır
+
+//! Fonksiyon tanımlama yöntemleri
+// Function Declaration (Fonksiyon Bildirimi):
+// Function Expression (Fonksiyon İfadesi):
+// Arrow Function (Ok Fonksiyonları):
+// Function Constructor (Fonksiyon Oluşturucusu):
+
+
+//! * --------Function-Declaration----------------------------*/ 
+// 1-Fonksiyonlar   function anahtar kelimesi ile tanımlanır
+// 2-Function kelimesini ardından fonksiyonun ismi verilir.
+// 3-Sonra da bir parantez açılıp kapatılır.
+//? fonksiyonun çalışması için çağırmak gereklidir.Çağırılmadığında işleme alınmaz.Bu çağırma işlemine invoke yada call denir.
