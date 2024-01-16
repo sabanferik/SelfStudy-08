67b73e360a7de5dc6bc9002aab3764c648984a2d
sabanferik <sbnftmaf@gmail.com>
2024-01-16 11:15:32 +0100

    String Methods Split

--- javascript/class-notes/js-10-String-Methods/string.js
+++ javascript/class-notes/js-10-String-Methods/string.js
@@ -119,7 +119,7 @@ console.log(s4.charAt(s4.length-1));
 
 
 // *=========================================================
-// *   includes()- case sensitive (büyük küçük harfe duyarlı)
+// !   includes()- case sensitive (büyük küçük harfe duyarlı)
 // *=========================================================
 
 const kelime="to be or not to be, that is The questions."
@@ -151,4 +151,96 @@ console.log(kelime1.indexOf("be",4));//16
 
 console.log(kelime1.indexOf("be",kelime1.indexOf("be")+1));//16
 
+// *=========================================================
+// *   search()  
+// *=========================================================
 
+const kelime2 = "to be or not to be, 5 that is The questions.";
+
+//!REGEX- regular expression
+
+console.log(kelime2.search(/[A-Z]/));//30. büyük harfi buldu ve index ini döndürdü
+console.log(kelime2.search(/[a-z]/));//0
+console.log(kelime2.search(/[0-9]/));//20
+console.log(kelime2.search(/[.]/));//43
+console.log(kelime2.indexOf("."));//43
+
+console.log(kelime2.search(/[^a-z]/));
+console.log(kelime2.search(/[^0-9a-zA-Z]/));// rakam küçük ve büyük harf dışındaki ilk karakteri döndür
+
+console.log(kelime.toUpperCase().search(/[aeuüıioö]/i));//i=incase sensitive, büyük küçük harfi önemseme
+
+//* ----------------------------------------------------------
+//! startsWith(), endsWith() -- case sensitive
+//* ----------------------------------------------------------
+
+const kelime4 = "Salına salına  sinsice olurum sana!";
+
+console.log(kelime4.startsWith("sa"));//false
+console.log(kelime4.startsWith("Sa"));//true
+
+console.log(kelime4.endsWith("sana!"));
+
+console.log(kelime4.startsWith("salına",7));//true 7. karakter ile başla 7 dahil
+
+console.log(kelime4.endsWith("salına",13));//true 13. karaktere kadar gel ama 13 dahil değil
+
+//* ----------------------------------------------------------
+//!  replace(searchFor, replaceWith) --immutable (değiştirmez)
+//* ----------------------------------------------------------
+
+let oku = "Oku Johny gibi saf olma saf olma saf olma";
+
+ oku= oku.replace("saf olma", "başarılı ol")
+console.log(oku);
+
+// Oku Johny gibi başarılı ol saf olma saf olma
+console.log(oku.replace(/SAF/i, "fakir"))
+
+//* /SAF/i şeklinde bir yazım incase sensitive (büyük küçük harfe takılma)
+
+console.log(oku.replace(/Saf olma/gi, "zengin ol"));
+
+//! gi ile yazılırsa hem bütün saf olma ları seçer, hem de incase sensitive
+
+//?ReplaceAll()
+let oku2 = "Oku Johny gibi SAF olma saf olma saf olma";
+
+console.log(oku2.replaceAll("saf olma", "akıllı ol"));
+
+
+//* ----------------------------------------------------------
+//!  slice(beginIndex, endIndex)
+//*  substring(beginIndex, endIndex)
+//* ----------------------------------------------------------
+
+const veysel = "UZUN İNCE BİR YOLDAYIM YÜRÜYORUM GUNDUZ GECE";
+
+
+console.log(veysel.slice(10));//BİR YOLDAYIM YÜRÜYORUM GUNDUZ GECE";
+console.log(veysel.slice(17,21));//DAYI. 21 dahil değil
+console.log(veysel.substring(17,21));//DAYI
+
+console.log(veysel.slice(-10));//UNDUZ GECE sondan saymaya 1 den başlıyoruz
+console.log(veysel.slice(-10,-6));//UNDU. yine -6 dahil değil
+
+console.log(veysel.slice(veysel.length-4,veysel.length-2));
+//! substring de - index çalışmaz
+console.log(veysel.substring(-10));
+
+//* ----------------------------------------------------------
+//* split= string i diziye çevirir
+//* split(seperator , limit ) =>ikisi de optional
+// //* split("x")=>x lerden ayırır, virgül+boşluk yapar ve yeni dizi döndürür.orjinal diziyi değiştirmez
+//* ----------------------------------------------------------
+
+
+const tarkan = "Gel gündüzle gece olalım";
+
+console.log(tarkan.split());// 1 elemanlı diziye çevirir
+console.log(tarkan.split(" "));// boşluklardan virgülle ayırılmış 4 elemanlı dizi
+console.log(tarkan.split(""));
+console.log(tarkan.split("e"));//e harflerinden virgülle ayrılan bir dizi
+ console.log(tarkan.split("e",3));//dizinin 3 elemanını al
+
+ console.log(tarkan);
