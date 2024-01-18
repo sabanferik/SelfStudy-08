7b5c2b726d2ddb7bc767b9fb7272050b0b89fb5f
sabanferik <sbnftmaf@gmail.com>
2024-01-18 10:50:37 +0100

    Push

--- javascript/class-notes/js-12-ARRAYS/array.js
+++ javascript/class-notes/js-12-ARRAYS/array.js
@@ -15,24 +15,24 @@
 
 // const car = ['Ford', 'Fiat', 'BMW']
 
-// // car = ['mercedes'] // hata verir
+// car = ['mercedes'] // hata verir
 
 // car[3] = 'Mercedes'
 
 
 // car[3] = 'Audi'
-// // console.log(car)
+// console.log(car)
 
 // car[10] = 'Mercedes'
 
 
 // car[8] = 'Ferrari'
-// // console.log(car)
+// console.log(car)
 
-// // console.log(car[9]) //undefined
-// // console.log(car[2]) //BMW
+// console.log(car[9]) //undefined
+// console.log(car[2]) //BMW
 
-// // console.log(car.length)
+// console.log(car.length)
 
 // console.log(car[car.length-1]) //Array in son elamanını getirir.
 
@@ -66,25 +66,87 @@
 // console.log(meyve3)
 
 
-let dob = 2000
+// let dob = 2000
 
-let color = 'red'
-color = 'yellow'
-
-
-
-const arr = [color,2024 - dob,'Yas']
-
-console.log(arr)
+// let color = 'red'
+// color = 'yellow'
 
 
-const arr2 = [color, 'Bir', [1,2,3,true,[]], {},false]
 
-console.log(arr2)
+// const arr = [color,2024 - dob,'Yas']
 
-console.log(arr2[2])
-console.log(arr2[2][3])
+// console.log(arr)
 
-arr2[2][4] = arr2
-console.log(arr2[2][4])
-console.log(arr2)
+
+// const arr2 = [color, 'Bir', [1,2,3,true,[]], {},false]
+
+// console.log(arr2)
+
+// console.log(arr2[2])
+// console.log(arr2[2][3])
+
+// arr2[2][4] = arr2
+// console.log(arr2[2][4])
+// console.log(arr2)
+
+
+//! Array Metotları
+
+//& Slice()
+
+/* const colors = ['Red','Green','Blue', 'Yellow','Brown']
+
+const colors2 = colors.slice() //parametre vermezsek tümünü kopyalar. */
+
+//colors[5] = 'White'  //colors2 yi etkilemez, değiştirmez
+
+
+// console.log(colors)
+// console.log(colors2)
+
+// const colors3 = colors.slice(2) //İkinci parametreyi vermezsek ilk parametrenin olduğu yerden sona kadar böler.
+// console.log(colors3)
+
+// const colors4 = colors.slice(2,4) // 2 den 4'e kadar kısmı alır. + hariçtir
+
+// console.log(colors4)
+
+/* console.log(colors[-1]) // undefined negatif indexle çağıramayız.
+
+const colors5 = colors.slice(-2)
+
+console.log(colors5)
+
+console.log(colors.slice(-4,-1)) //Green, blue yellow
+console.log(colors.slice(-2,-4)) // limit dışı boş array
+console.log(colors.slice(2,-2)) // Blue
+console.log(colors.slice(-2)) // Son iki elemanı getiri */
+
+//& Push() Array in sonuna elaman ekler ve yeni arrayin elaman sayısını return eder.
+
+
+const colors = ['Red','Green']
+
+// let newLen = colors.push('Blue')
+
+// console.log(newLen) //3
+
+// console.log(colors.push('Yellow')) //4
+
+
+// console.log(colors.push()) // length ile aynı oldu
+// console.log(colors.push() * 5 ) 
+
+// colors.push('Blue','pink') // verdiğimiz sıra ile sona ekler
+// console.log(colors)
+
+
+const colors2 = ['Blue','Yellow']
+
+// colors.push(colors2) // ['Red','Green',['Blue','Yellow']]
+
+
+colors.push(...colors2) //spread parametresi ile tek tek ekler
+
+console.log(colors) // ['Red','Green','Blue','Yellow']
+
