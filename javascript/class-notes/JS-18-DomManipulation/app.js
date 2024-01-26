197832f4f33b5f0561f4e5ec21692fb59941e7a6
sabanferik <sbnftmaf@gmail.com>
2024-01-26 12:38:41 +0100

    Dom input kisayol son

--- javascript/class-notes/JS-18-DomManipulation/app.js
+++ javascript/class-notes/JS-18-DomManipulation/app.js
@@ -49,4 +49,82 @@ resim.addEventListener("mouseout",()=>{
   
     resim.src = "./img/aslan1.jpeg";
     document.querySelector(".ses2").pause();
-})
+});
+
+//! klavyeden inputa veri girisi yapilirken elimizi tustan cektigimizde varolan degisiklik
+
+const textInput=document.querySelector(".textbox")
+const checkInput=document.querySelector(".checkbox")
+
+console.log(textInput);
+
+textInput.onkeyup=()=>{
+    
+// if(checkInput.checked){
+
+// textInput.value=textInput.value.toUpperCase()
+// }else{
+
+// textInput.value=textInput.value.toLowerCase()
+// }
+
+checkInput.checked 
+? textInput.value=textInput.value.toUpperCase()
+: textInput.value=textInput.value.toLowerCase()
+
+}
+
+
+//! listemin başına "Programlama Dilleri" h1 etiketi eklemek ****
+//!uzun yol*****
+
+//? HTML DE h1 elementi oluştur
+
+const baslik= document.createElement("h1")
+
+{
+    /* <h1></h1> */
+  }
+  
+  //? oluşan h1 elementine class ismi ata
+  
+  baslik.className="baslik2"
+
+  {
+    /* <h1 class="baslik2"></h1> */
+  }
+  
+  //?Programlama Dilleri text i oluştur
+  
+  const yazi= document.createTextNode("Programlama Dilleri")
+
+  //? h1 elementi için oluşturduğum text i h1 e child yapalım
+  
+  baslik.appendChild(yazi)
+  
+  {
+    /* <h1 class="baslik2"> Programlama Dilleri</h1> */
+  }
+  
+  const inputdiv= document.querySelector(".input-div")
+  //? input-div class isimli div in son child ı yapalım
+
+  inputdiv.appendChild(baslik)
+  
+  //? input-div class isimli div den hemen sonraya ekle
+
+  inputdiv.after(baslik)
+//!!  "append" metodu ile seçtiğimiz Html etiketi bitmeden önceki son kısma ekleme yapabiliriz (<div>merhaba -buraya-  </div>).  "prepend" metodu ile seçtiğimiz Html etiketi başladıktan sonraki ilk kısma ekleme yapabiliriz  (<div> -buraya-  merhaba   </div>).  "after" metodu ile ile seçtiğimiz Html etiketi bittikten sonraki ilk kısma ekleme yapabiliriz (<div> merhaba </div>  -buraya-). before ile de etiketin önüne ekleyebiliriz
+//*--------------------*/
+//!kisayol (backtick)****
+
+// const inputdiv= document.querySelector(".input-div")
+
+// inputdiv.innerHTML =inputdiv.innerHTML+ h1
+// inputdiv.innerHTML += `<h1 class="title text-primary">Programlama Dilleri</h1>`
+
+//!  kısayol ile daha önce event yapılmış bir tag in içeriğine ekleme yaparsak hata alabiliriz. bu yüzden yeri olmayan bir etiketi kısayol ile eklemek istiyorsak, html de onun için boş bir tag açmakta fayda var
+
+document.querySelector(
+    ".mecburen"
+    ).innerHTML += `<h1 class="title text-primary">Programlama Dilleri</h1>`;
