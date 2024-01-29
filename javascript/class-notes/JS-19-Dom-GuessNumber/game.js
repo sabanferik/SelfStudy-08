5f01107e13b357f73ba4f01d872c54e4010a1c08
sabanferik <sbnftmaf@gmail.com>
2024-01-29 11:20:55 +0100

    check butonuna basilinca olacaklar

---
+++ javascript/class-notes/JS-19-Dom-GuessNumber/game.js
@@ -0,0 +1,15 @@
+ 
+//*pc ye sayı tutturduk, 1-20 arasında olabilmesi için 20 ile çarpıp yukarı yuvarladık
+ 
+ let rastgeleNumberPC=Math.ceil(Math.random()*20);
+
+ console.log(rastgeleNumberPC);
+
+ let message=document.querySelector(".msg")
+
+ let skor=10
+ let enYuksekSkor=0
+
+//!her check butonuna basilinca olacaklar
+
+ 
