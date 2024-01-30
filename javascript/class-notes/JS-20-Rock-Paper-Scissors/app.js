10367b58d29d1b24a7224d22fc9781f777abbf83
sabanferik <sbnftmaf@gmail.com>
2024-01-30 10:34:03 +0100

    fonksiyonlar öncesi

--- javascript/class-notes/JS-20-Rock-Paper-Scissors/app.js
+++ javascript/class-notes/JS-20-Rock-Paper-Scissors/app.js
@@ -3,6 +3,14 @@ const yourChoiceDiv = document.getElementById('your-choice')
 const pcChoiceDiv = document.getElementById('pc-choice')
 const selectionArticle = document.querySelector('.selection')
 
+const messagePar = document.querySelector('.message')
+
+//& Score
+const scoreCardSection = document.querySelector('.score-card')
+const yourScoreSpan = document.getElementById('your-score')
+const pcScoreSpan = document.getElementById('pc-score')
+const domTopScore = document.getElementById('top-score')
+
 //& Değişkenler
 
 let userSelection;
@@ -10,6 +18,11 @@ let pcRandom;
 let pcArr = [];
 const userSelectImg = document.createElement('img');
 const pcSelectImg = document.createElement('img');
+
+//& Colors
+const YELLOW = "#ffc538";
+const RED = "#fb778b";
+const GREEN = "#5ab7ac";
 
 // console.log(selectionArticle)
 
@@ -34,12 +47,30 @@ selectionArticle.addEventListener('click',(e)=>{
 const createPcSelection = ()=>{
     pcArr = ['rock', 'paper', 'scissor','rock', 'paper', 'scissor'];
     pcRandom = pcArr[Math.trunc(Math.random()*6)]// daha fazla secim alternatifi olsun diye iki defa yazildi
-    console.log(pcRandom)
+    // console.log(pcRandom)
     pcSelectImg.src =`./assets/${pcRandom}.png`;
     pcSelectImg.id = `pc`;
     pcChoiceDiv.appendChild(pcSelectImg)
+
+    calculateResult()
 }
 
+const calculateResult = ()=>{
+    // console.log(userSelection)
+    // console.log(pcRandom)
+    if(userSelection === pcRandom){
+        draw()
+    }else {
+        if(userSelection=== 'rock'){
+            pcRandom==='paper' ? youLost() : youWin()
+        }else if(userSelection=== 'paper'){
+            pcRandom==='scissor' ? youLost() : youWin()
+        }else if(userSelection=== 'scissor'){
+            pcRandom==='rock' ? youLost() : youWin()
+        }
+    }
+    
+ }
 
 
 
