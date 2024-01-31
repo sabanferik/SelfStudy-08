4bd683152bcb33b9db9cfdc4756d27390a7e5637
sabanferik <sbnftmaf@gmail.com>
2024-01-31 08:52:24 +0100

    IOS Calculater klasör ve dosya

---
+++ javascript/class-notes/JS-21-Ios-Calculator/solution/app.js
@@ -0,0 +1,104 @@
+//* ======================================================
+//*                     IOS CALCULATOR
+//* ======================================================
+
+
+
+
+
+
+
+//?operator değişkenleri
+
+
+
+
+// //?eşittir yada percent e basıldıktan sonra yeni işleme yeni sayılar girmek için, tıklandı tıklanmadı boolean değişkeni hazırladık, eşittir (ve de percent) butonunda bu true yani tıklandı olacak
+
+
+
+
+//?*********** herhangi bir number a basılınca
+
+
+
+
+
+
+//!EKRANA HAZIRLIK
+
+  
+
+// //?kullanıcı 0 girerse, sonrasında 0 ve . dışında bir sayı girerse, ekranda sadece girilen yeni sayı (0 iptal olsun) gözüksün
+
+
+
+
+
+//?kullanıcı herhangi bir yerde . girmişken, tekrar nokta girmeye kalkarsa giremesin
+
+
+//?kullanıcı 10 haneden sonra girmesin
+
+
+
+
+//?kullanıcı ilk başta 0 girer ardından tekrar 0 girerse, girilmesin, tek 0 döndürsün
+
+
+// //?eşittir yada percent a basıldıktan sonra girilen number tek başına ekranda görünsün,çünkü yeni işlem başlıyor(ekranda 72 yazan işlem sonucu varken 5 e basınca 725 olmasın)
+
+
+
+
+//?bütün şartları başarı ile geçtiyse basılan numaraları arka arkaya ekle
+
+
+//!BURADA YAPILANLARI EKRANA BASTIRMA
+
+
+
+  //? işlem sonucu 8 haneyi geçmesin
+
+  //?işlem girilince
+
+
+
+
+//?**************HERHANGİ BİR İŞLEME TIKLANDIĞINDA
+
+
+
+//?currentDisplay boşken, hiçbir şekilde sayı girişi yapılmamışsa, operatöre basılmasın. boş return yapmaya çalıştığınız işlemi yaptırmaz.
+//? return fonksiyon içerisinde her yerde kullanılabilir. Kod return satırına eriştiğinde fonksiyon durur ve değer fonksiyonun çağırıldığı yere geri gönderilir. Bir fonksiyon içerisinde birden fazla return fonksiyonu da olabilir. return değer döndürmek zorunda değildir.
+
+
+
+
+    //?eşittire basılmadan arka arkaya işleme basılırsa (alt ve üst ekran doluyken işleme basılmaya devam edilirse)
+
+
+
+
+//?**************eşittir butonuna tıklandığında
+
+
+
+
+
+
+//! HESAPLA FONKSİYONU
+
+
+
+
+
+
+
+
+//?AC butonuna basıldığında
+
+
+
+//? PM butonuna basıldığında
+
