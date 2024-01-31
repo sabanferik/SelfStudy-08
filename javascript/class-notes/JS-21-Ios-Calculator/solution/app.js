e9a778ef71c5655c235c261bc73fd6da835dc6d6
sabanferik <sbnftmaf@gmail.com>
2024-01-31 11:19:10 +0100

    ikinci ders sonu

--- javascript/class-notes/JS-21-Ios-Calculator/solution/app.js
+++ javascript/class-notes/JS-21-Ios-Calculator/solution/app.js
@@ -11,7 +11,6 @@ const currentDisplay = document.querySelector(".current-display");
 
 
 
-
 //?operator değişkenleri
 let ustEkranText=""
 let altEkranText=""
@@ -48,19 +47,27 @@ const ekranaHazirlik=(num)=>{
 
 //todo kullanıcı 0 girerse, sonrasında 0 ve . dışında bir sayı girerse, ekranda sadece girilen yeni sayı (0 iptal olsun) gözüksün
 
-
+if(altEkranText==="0" && num!=="0" && num!=="."){
+    altEkranText=num
+}
 
 
 
 //todo kullanıcı herhangi bir yerde . girmişken, tekrar nokta girmeye kalkarsa giremesin
 
+if(num="." && altEkranText.includes(".")) return
+
 
 //todo kullanıcı 10 haneden sonra girmesin
+
+if(altEkranText.length>9) return
 
 
 
 
 //todo kullanıcı ilk başta 0 girer ardından tekrar 0 girerse, girilmesin, tek 0 döndürsün
+
+if(altEkranText==="0" && num==="0") return
 
 
 //todo eşittir yada percent a basıldıktan sonra girilen number tek başına ekranda görünsün,çünkü yeni işlem başlıyor(ekranda 72 yazan işlem sonucu varken 5 e basınca 725 olmasın)
@@ -92,11 +99,10 @@ currentDisplay.textContent=altEkranText
 
 if(islem){
 
- //üstekranda alttaki sayı +işlem
+ previousDisplay.textContent=`${ustEkranText} ${islem}`
 
- previousDisplay.textContent=`${ustEkranText} ${islem}` //ustEkranText+islem
 }else{
- //hiçbişey olmasın
+ previousDisplay.textContent=""
 }
 
 
@@ -104,39 +110,66 @@ if(islem){
 }
 //?**************HERHANGİ BİR İŞLEME TIKLANDIĞINDA
 
-operationButtons.forEach((op)=>{
-  op.onclick=()=>{
-    
+operationButtons.forEach((op) => {
+ op.onclick = () => {
 
 
-
-
-//?currentDisplay boşken, hiçbir şekilde sayı girişi yapılmamışsa, operatöre basılmasın. boş return yapmaya çalıştığınız işlemi yaptırmaz.
-//? return fonksiyon içerisinde her yerde kullanılabilir. Kod return satırına eriştiğinde fonksiyon durur ve değer fonksiyonun çağırıldığı yere geri gönderilir. Bir fonksiyon içerisinde birden fazla return fonksiyonu da olabilir. return değer döndürmek zorunda değildir. return den sonra else yerine if kullanmalıyız
-
-
+   //?currentDisplay boşken, hiçbir şekilde sayı girişi yapılmamışsa, operatöre basılmasın. boş return yapmaya çalıştığınız işlemi yaptırmaz.
+   //? return fonksiyon içerisinde her yerde kullanılabilir. Kod return satırına eriştiğinde fonksiyon durur ve değer fonksiyonun çağırıldığı yere geri gönderilir. Bir fonksiyon içerisinde birden fazla return fonksiyonu da olabilir. return değer döndürmek zorunda değildir. return den sonra else yerine if kullanmalıyız
 
 
    //todo eşittire basılmadan arka arkaya işleme basılırsa (alt ve üst ekran doluyken işleme basılmaya devam edilirse)
 
-   islem=op.textContent
 
-    ustEkranText=altEkranText
-    altEkranText=""
-    updateEkran()
-  };
+    islem=op.textContent
 
-})
+  ustEkranText=altEkranText
+  altEkranText=""
+updateEkran()
+
+ };
+});
 //?**************eşittir butonuna tıklandığında
 
+equalButtons.onclick=()=>{
+//eşittire basınca istenilen işlem yapılması için fonksiyon
+hesapla()
 
+//buradaki değişkenlerle yapılna işlemlerin sonucu ekrana yansıtılsın
+updateEkran()
+
+
+}
 
 
 
 
 //! HESAPLA FONKSİYONU
 
+const hesapla=()=>{
 
+switch (islem) {
+ case "+":
+  sonuc= +ustEkranText+ Number(altEkranText)
+   break;
+ case "-":
+
+sonuc= ustEkranText - altEkranText;
+   break;
+ case "x":
+ sonuc=  ustEkranText * altEkranText;
+   break;
+ case "÷":
+ sonuc=  ustEkranText / altEkranText;
+   break;
+ default:
+   break;
+}
+altEkranText=sonuc
+ustEkranText=""
+islem=""
+
+}
 
 
 
