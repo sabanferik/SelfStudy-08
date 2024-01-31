1ad41e9f0169001117a9ad98e345e5a2c3a59dc9
sabanferik <sbnftmaf@gmail.com>
2024-01-31 11:41:10 +0100

    düzeltme

--- javascript/class-notes/JS-21-Ios-Calculator/solution/app.js
+++ javascript/class-notes/JS-21-Ios-Calculator/solution/app.js
@@ -2,183 +2,127 @@
 //*                     IOS CALCULATOR
 //* ======================================================
 
-const numberButtons= document.querySelectorAll(".num")
-const operationButtons= document.querySelectorAll(".operator")
-const equalButtons=document.querySelector(".equal")
-const previousDisplay=document.querySelector(".previous-display")
+const numberButtons = document.querySelectorAll(".num");
+const operationButtons = document.querySelectorAll(".operator");
+const equalButtons = document.querySelector(".equal");
+const previousDisplay = document.querySelector(".previous-display");
 
 const currentDisplay = document.querySelector(".current-display");
 
-
-
 //?operator değişkenleri
-let ustEkranText=""
-let altEkranText=""
-let islem=""
-
+let ustEkranText = "";
+let altEkranText = "";
+let islem = "";
 
 //todo eşittir yada percent e basıldıktan sonra yeni işleme yeni sayılar girmek için, tıklandı tıklanmadı boolean değişkeni hazırladık, eşittir (ve de percent) butonunda bu true yani tıklandı olacak
 
-
-
-
 //?*********** herhangi bir number a basılınca
 
-numberButtons.forEach((number)=>{
+numberButtons.forEach((number) => {
+  number.onclick = () => {
+    ekranaHazirlik(number.textContent);
 
- number.onclick=()=>{
-ekranaHazirlik(number.textContent)
-
-updateEkran()
- }
-
-
-})
-
-
-
+    updateEkran();
+  };
+});
 
 //!EKRANA HAZIRLIK
 
-const ekranaHazirlik=(num)=>{
- 
-
-
-
-//todo kullanıcı 0 girerse, sonrasında 0 ve . dışında bir sayı girerse, ekranda sadece girilen yeni sayı (0 iptal olsun) gözüksün
+const ekranaHazirlik = (num) => {
+  //todo kullanıcı 0 girerse, sonrasında 0 ve . dışında bir sayı girerse, ekranda sadece girilen yeni sayı (0 iptal olsun) gözüksün
 
 if(altEkranText==="0" && num!=="0" && num!=="."){
-    altEkranText=num
+ 
+  altEkranText=num
 }
 
 
+  //todo kullanıcı herhangi bir yerde . girmişken, tekrar nokta girmeye kalkarsa giremesin
 
-//todo kullanıcı herhangi bir yerde . girmişken, tekrar nokta girmeye kalkarsa giremesin
-
-if(num="." && altEkranText.includes(".")) return
-
-
-//todo kullanıcı 10 haneden sonra girmesin
-
-if(altEkranText.length>9) return
+if(num=="." && altEkranText.includes(".")) return
 
 
+  //todo kullanıcı 10 haneden sonra girmesin
 
+  if(altEkranText.length > 9) return
 
-//todo kullanıcı ilk başta 0 girer ardından tekrar 0 girerse, girilmesin, tek 0 döndürsün
+  //todo kullanıcı ilk başta 0 girer ardından tekrar 0 girerse, girilmesin, tek 0 döndürsün
 
-if(altEkranText==="0" && num==="0") return
+  if(altEkranText=="0" && num=="0") return
+  //todo eşittir yada percent a basıldıktan sonra girilen number tek başına ekranda görünsün,çünkü yeni işlem başlıyor(ekranda 72 yazan işlem sonucu varken 5 e basınca 725 olmasın)
 
+  //?bütün şartları başarı ile geçtiyse basılan numaraları arka arkaya ekle
 
-//todo eşittir yada percent a basıldıktan sonra girilen number tek başına ekranda görünsün,çünkü yeni işlem başlıyor(ekranda 72 yazan işlem sonucu varken 5 e basınca 725 olmasın)
-
-
-
-
-//?bütün şartları başarı ile geçtiyse basılan numaraları arka arkaya ekle
-
-altEkranText+=num
-
-
-}
-
-
-
+  altEkranText += num;
+};
 
 //!BURADA YAPILANLARI EKRANA BASTIRMA
 
-const updateEkran=()=>{
+const updateEkran = () => {
+  currentDisplay.textContent = altEkranText;
 
-currentDisplay.textContent=altEkranText
+  //todo işlem sonucu 8 haneyi geçmesin
 
- //todo işlem sonucu 8 haneyi geçmesin
+  //?işlem girilince
 
-
-
- //?işlem girilince
-
-if(islem){
-
- previousDisplay.textContent=`${ustEkranText} ${islem}`
-
-}else{
- previousDisplay.textContent=""
-}
-
-
-
-}
+  if (islem) {
+    previousDisplay.textContent = `${ustEkranText} ${islem}`;
+  } else {
+    previousDisplay.textContent = "";
+  }
+};
 //?**************HERHANGİ BİR İŞLEME TIKLANDIĞINDA
 
 operationButtons.forEach((op) => {
- op.onclick = () => {
+  op.onclick = () => {
+    //?currentDisplay boşken, hiçbir şekilde sayı girişi yapılmamışsa, operatöre basılmasın. boş return yapmaya çalıştığınız işlemi yaptırmaz.
+    //? return, fonksiyon içerisinde her yerde kullanılabilir. Kod return satırına eriştiğinde fonksiyon durur ve değer fonksiyonun çağırıldığı yere geri gönderilir. Bir fonksiyon içerisinde birden fazla return fonksiyonu da olabilir. return değer döndürmek zorunda değildir. return den sonra else if yerine if tercih etmeliyiz
 
+    if(altEkran==="") return
 
-   //?currentDisplay boşken, hiçbir şekilde sayı girişi yapılmamışsa, operatöre basılmasın. boş return yapmaya çalıştığınız işlemi yaptırmaz.
-   //? return fonksiyon içerisinde her yerde kullanılabilir. Kod return satırına eriştiğinde fonksiyon durur ve değer fonksiyonun çağırıldığı yere geri gönderilir. Bir fonksiyon içerisinde birden fazla return fonksiyonu da olabilir. return değer döndürmek zorunda değildir. return den sonra else yerine if kullanmalıyız
+    //todo eşittire basılmadan arka arkaya işleme basılırsa (alt ve üst ekran doluyken işleme basılmaya devam edilirse)
 
+    islem = op.textContent;
 
-   //todo eşittire basılmadan arka arkaya işleme basılırsa (alt ve üst ekran doluyken işleme basılmaya devam edilirse)
-
-
-    islem=op.textContent
-
-  ustEkranText=altEkranText
-  altEkranText=""
-updateEkran()
-
- };
+    ustEkranText = altEkranText;
+    altEkranText = "";
+    updateEkran();
+  };
 });
 //?**************eşittir butonuna tıklandığında
 
-equalButtons.onclick=()=>{
-//eşittire basınca istenilen işlem yapılması için fonksiyon
-hesapla()
+equalButtons.onclick = () => {
+  //eşittire basınca istenilen işlem yapılması için fonksiyon
+  hesapla();
 
-//buradaki değişkenlerle yapılna işlemlerin sonucu ekrana yansıtılsın
-updateEkran()
-
-
-}
-
-
-
+  //buradaki değişkenlerle yapılna işlemlerin sonucu ekrana yansıtılsın
+  updateEkran();
+};
 
 //! HESAPLA FONKSİYONU
 
-const hesapla=()=>{
-
-switch (islem) {
- case "+":
-  sonuc= +ustEkranText+ Number(altEkranText)
-   break;
- case "-":
-
-sonuc= ustEkranText - altEkranText;
-   break;
- case "x":
- sonuc=  ustEkranText * altEkranText;
-   break;
- case "÷":
- sonuc=  ustEkranText / altEkranText;
-   break;
- default:
-   break;
-}
-altEkranText=sonuc
-ustEkranText=""
-islem=""
-
-}
-
-
-
-
-
+const hesapla = () => {
+  switch (islem) {
+    case "+":
+      sonuc = +ustEkranText + Number(altEkranText);
+      break;
+    case "-":
+      sonuc = ustEkranText - altEkranText;
+      break;
+    case "x":
+      sonuc = ustEkranText * altEkranText;
+      break;
+    case "÷":
+      sonuc = ustEkranText / altEkranText;
+      break;
+    default:
+      break;
+  }
+  altEkranText = sonuc;
+  ustEkranText = "";
+  islem = "";
+};
 
 //?AC butonuna basıldığında
 
-
-
 //? PM butonuna basıldığında
-
