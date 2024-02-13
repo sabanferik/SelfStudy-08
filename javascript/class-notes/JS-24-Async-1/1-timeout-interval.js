7d268d4b2fe2009912f394f5cc0b356957fcd181
sabanferik <sbnftmaf@gmail.com>
2024-02-13 10:03:30 +0100

    ASYNC - time out-interval son

--- javascript/class-notes/JS-24-Async-1/1-timeout-interval.js
+++ javascript/class-notes/JS-24-Async-1/1-timeout-interval.js
@@ -20,14 +20,65 @@
 //****************************************** */
 
 
-const bekle = ()=>{
+// const bekle = (ms)=>{
 
 
-    const start= new Date().getTime()
+//     const start= new Date().getTime()
+
+//     while(new Date().getTime() < start+ms){}
 
 
-}
+// }
 
-console.time("timer1");
-bekle(3000)
-console.timeEnd("timer1")
+// console.time("timer1");
+// bekle(3000)
+// console.timeEnd("timer1")
+
+
+//*Asenkron (setTimeout) 1 seferlik
+//******************************** */
+
+// setTimeout(()=>{
+
+// console.log("aleyküm selam");
+
+// },2000)
+
+// console.log("selamun aleyküm");
+
+// setTimeout(()=>{
+//     console.log("naber");
+//     },3000)
+
+//!Selam Aleyküm -Aleyküm Selamun - Naber napıyorsun çalışır, 3. nün süresi 1.den azsa, 2-3-1 sırasında çalışır
+//!settimeout müsait zamanda ortaya çıkar, onun sırası geçtiğinde alttaki işlemler bitince çalışır  
+
+//*Asenkron(setInterval, clearInterval) belli aralıklarla durdurulana kadar devam eder. mesela 1 dakikada bir döviz kurunu güncellemek için kullanılabilir
+//***************************** */
+
+//?setInterval non-blocking (kod burada takılı kalmaz alttaki kodlarda çalışır)
+
+// let sayac=0
+// const interval1=setInterval(()=>{
+//     console.log(++sayac);
+
+
+//     if(sayac==6){
+//         //!interval i durdurma kod blogu
+//         clearInterval(interval1)
+//     }
+
+
+// },1000)
+
+// //? calback hell
+
+// setTimeout(() => {
+//     console.log("ayse:s.a. ahmet");
+//     setTimeout(() => {
+//       console.log("ahmet:a.s. ayse nasılsın");
+//       setTimeout(() => {
+//         console.log("ayse:iyiyim sağol");
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
