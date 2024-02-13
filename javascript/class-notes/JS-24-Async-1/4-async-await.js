860ae75886a83f8096688fdc5a8e0a431044a4e3
sabanferik <sbnftmaf@gmail.com>
2024-02-13 11:26:21 +0100

    await ders sonu

--- javascript/class-notes/JS-24-Async-1/4-async-await.js
+++ javascript/class-notes/JS-24-Async-1/4-async-await.js
@@ -22,3 +22,14 @@
 
 const defaultImage =
   "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/220px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg";
+
+  const getData=async()=>{
+
+   const response= await fetch("https://api.tvmaze.com/search/shows?q=girls");
+   const veri= await response.json()
+
+   ekranaBastir(veri)
+
+  }
+
+  getData()
