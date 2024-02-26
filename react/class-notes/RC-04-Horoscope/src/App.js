587391870943234501372fe2b8576d43cde03994
sabanferik <sbnftmaf@gmail.com>
2024-02-26 10:58:09 +0100

    Horoscope Ashley hc self study ilk

---
+++ react/class-notes/RC-04-Horoscope/src/App.js
@@ -0,0 +1,18 @@
+import "./App.scss";
+
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div>
+     
+    <Navbar/>
+
+    <Header/>
+    </div>
+  );
+}
+
+export default App;
