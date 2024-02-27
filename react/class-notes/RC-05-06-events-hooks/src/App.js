fe7d0501400dfadc6e848af7837bb515d59fc8d9
sabanferik <sbnftmaf@gmail.com>
2024-02-27 09:51:53 +0100

    SelfStudy React Event son

---
+++ react/class-notes/RC-05-06-events-hooks/src/App.js
@@ -0,0 +1,15 @@
+
+
+import React from "react";
+import Events from "./1-events-hookIntro/Events";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div>
+     
+    <Events/>
+    </div>
+  );
+}
+
+export default App;
