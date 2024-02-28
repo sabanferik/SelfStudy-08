80148500883f61a762c440cf5ce9d258c178b105
sabanferik <sbnftmaf@gmail.com>
2024-02-28 13:32:06 +0100

    Gün sonu

--- react/class-notes/RC-05-06-events-hooks/src/3-Form/Form.jsx
+++ react/class-notes/RC-05-06-events-hooks/src/3-Form/Form.jsx
@@ -1,75 +1,78 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { FaGooglePlus, FaReact } from "react-icons/fa";
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+const MouseKeyboard = () => {
+  const [xEkseni, setXekseni] = useState(0);
+  const [yEkseni, setYekseni] = useState(0);
 
-const Form = () => {
-  const [isim, setIsim] = useState("");
-  const [pass, setPass] = useState("");
-  const [country, setCountry] = useState("");
+  const [inputData, setInputData] = useState("");
 
-  const getDatabase = (e) => {
-    e.preventDefault()
-    //? ileride bir submit islemin neticesinde verilerin nasıl post edilebileceğini gostermek adina eklenmistir.
-    //? const res = axios.post("url", { isim, pass, country })
+  //!onMouseMove=mouse umun sayfa üzerindeki hareketlerini inceler. pageX ve pageY komutunu kullanırsanız, bulunduğunuz sayfanın etiketlerinin sol üst köşesini 0-0 kabul eder, ona göre konum bildirir
 
-    alert(`name:${isim}
-       password: ${pass}
-       ülkeler: ${country} `);
-//!database e yollandıktan sonra değişkenlerin içini temizledik, bunu ekranda da temiz görmek için inputlarda value değişkenleri tanımladık
-      setIsim("")
-      setPass("")
-      setCountry("")
+  // pageX/Ykoordinatlar, oluşturulan sayfanın tamamının sol üst köşesine göredir (kaydırılarak gizlenen parçalar dahil),
+
+  // clientX/YKoordinatlar sayfanın görünen kısmının sol üst köşesine göredir ve tarayıcı penceresinden "görünür" .
+
+  // screenX/Yfiziksel ekrana göredir.
+
+  const mouseEvent = (e) => {
+    // console.log(e.pageX);
+    setXekseni(e.pageX);
+    // setXekseni(e.screenX);
+    setYekseni(e.pageY);
   };
+
+
+  const keyboardEvent=(e)=>{
+    console.log(e);
+if(e.keyCode >=48 && e.keyCode <= 57){
+    alert("tebrikler rakam girdiniz")
+}else{
+    alert("hatalı giriş yaptınız lütfen rakam giriniz")
+}
+
+  }
+
+
   return (
-    <div className="container text-center mt-4">
-      <h1>*********************************************</h1>
+    <div
+      onMouseMove={mouseEvent}
+      className="container text-center mt-4 d-flex flex-column align-items-center"
+    >
+      <h2 className="text-danger">MOUSE EVENTS</h2>
+      <p>X ve Y</p>
+      <p className="text-danger fw-bold">
+        {xEkseni} - {yEkseni}
+      </p>
+      <div
+        className="bg-success text-light w-50 p-4"
+        id="coord"
+        onMouseMove={mouseEvent}
+      >
+        <HiOutlineDesktopComputer /> KOORDİNATLAR <FaReact /> <FaGooglePlus />
+      </div>
 
-      <h1>FORM(EVENTS)</h1>
+      <div>
+        <h2 className="text-danger mt-4">
+          Keyboard-Clipboard Events{" "}
+          <span className="text-primary">{inputData}</span>
+        </h2>
 
-      <form onSubmit={getDatabase}>
-        <div className="mb-3">
-          <label className="form-label" htmlFor="isim">
-            NAME: <span className="text-danger">{isim} </span>
-          </label>
-          <input
-            className="form-control"
-            id="isim"
-            type="text"
-            onInput={(a) => setIsim(a.target.value)}
-            value={isim}
-          />
-        </div>
-        <div className="mb-3">
-          <label className="form-label" htmlFor="password">
-            PASSWORD
-          </label>
-          <input
-            className="form-control"
-            id="password"
-            type="password"
-            onChange={(e) => setPass(e.target.value)}
-            value={pass}
-          />
-        </div>
+        <input
+          type="text"
+          className="form-control"
+          onChange={(e) => setInputData(e.target.value)}
+          //   onKeyDown={(e)=>(e.keyCode>=48 && e.keyCode<=57) ? alert("tebrik"):alert("hata")}
+          onKeyDown={keyboardEvent}
+        />
+      </div>
 
-        <div className="mb-3">
-          <label className="form-label" htmlFor="country">
-            COUNTRY: <span className="text-danger">{country} </span>
-          </label>
-          <select
-            onChange={(e) => setCountry(e.target.value)}
-            className="form-select"
-          >
-            <option selected>COUNTRIES</option>
-            <option value="Türkiye">TURKEY</option>
-            <option value="Almanya">GERMANY</option>
-            <option value="Amerika">USA</option>
-          </select>
-        </div>
-        <button type="submit" className="btn btn-primary">
-          GÖNDER (submit)
-        </button>
-      </form>
+      {/* onmouseenter genellikle fare işaretçisi bir öğeden ayrıldığında meydana gelen
+      olayla (onmouseleave) birlikte kullanılır .  onmouseenter,onmouseover olayına benzer  . Aradaki fark, onmouseenter
+      olayının
+      bubble olmamasıdır (belge hiyerarşisini yukarıya taşımaz, child a ve parent a aynı olayı verdiğinizde herkes kendi alanında çalışır, child dan parent etkilenmez) */}
     </div>
   );
 };
 
-export default Form;
+export default MouseKeyboard;
