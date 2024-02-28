6cb9d08604ccff0da67f85384d240707c54bdcd1
sabanferik <sbnftmaf@gmail.com>
2024-02-28 09:22:14 +0100

    Form(Events) label

--- react/class-notes/RC-05-06-events-hooks/src/3-Form/Form.jsx
+++ react/class-notes/RC-05-06-events-hooks/src/3-Form/Form.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const Form = () => {
   return (
-    <div>Form</div>
-  )
-}
+    <div className="container text-center mt-4">
+      <h1>*********************************************</h1>
 
-export default Form
+      <h1>FORM(EVENTS)</h1>
+
+      <form>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="isim">
+        NAME: <span></span>
+          </label>
+          <input className="form-control" id="isim" type="text" />
+        </div>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="password">PASSWORD</label>
+          <input className="form-control" id="password" type="password" />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label" htmlFor="country">
+         COUNTRY: <span></span>
+          </label>
+         <select  className="form-select">
+            <option selected>COUNTRIES</option>
+            <option >TURKEY</option>
+            <option >GERMANY</option>
+            <option >USA</option>
+         </select>
+
+        </div>
+<button type="submit" className="btn btn-primary">GÖNDER (submit)</button>
+
+      </form>
+    </div>
+  );
+};
+
+export default Form;
