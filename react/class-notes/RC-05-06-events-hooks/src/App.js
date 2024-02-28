b2d5dcb5f2be0194ea37f11b2bf80ff6ea4f358a
sabanferik <sbnftmaf@gmail.com>
2024-02-28 09:12:20 +0100

    Form hazirlik

--- react/class-notes/RC-05-06-events-hooks/src/App.js
+++ react/class-notes/RC-05-06-events-hooks/src/App.js
@@ -5,16 +5,17 @@ import Events from "./1-events-hookIntro/Events";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Hooks from "./2-functionalCompHooks/Hooks";
 import Counter from "./2-classComponents/Counter";
+import Form from "./3-Form/Form";
 function App() {
   return (
     <div>
      
-    <Events/>
-    <Hooks/>
+    {/* <Events/> */}
+    {/* <Hooks/> */}
 
-    <Counter/>
-    <Counter count={20}/>
-
+    {/* <Counter/>
+    <Counter count={20}/> */}
+    <Form/>
     </div>
   );
 }
