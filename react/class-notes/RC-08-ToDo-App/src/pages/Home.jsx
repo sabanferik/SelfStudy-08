7b05957563da383f933f63b444c93e7b59ea75a8
sabanferik <sbnftmaf@gmail.com>
2024-03-05 10:25:27 +0100

    first lesson end

--- react/class-notes/RC-08-ToDo-App/src/pages/Home.jsx
+++ react/class-notes/RC-08-ToDo-App/src/pages/Home.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import GorevEkle from "../components/GorevEkle";
 import GorevleriGoster from "../components/GorevleriGoster";
 
-
+import Data from "../helper/Data"
 
 const Home = () => {
 
-
+const[todos,setTodos]=useState(Data)
  
   return (
     <div>
-     home
+     {/* <GorevEkle/> */}
+     <GorevleriGoster todos={todos} setTodos={setTodos}/>
     </div>
   );
 };
