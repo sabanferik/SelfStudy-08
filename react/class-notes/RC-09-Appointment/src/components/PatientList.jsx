c79bdf02861afaa05eec4e57a17469be737a06df
sabanferik <sbnftmaf@gmail.com>
2024-03-07 10:34:46 +0100

    Patient list isDone

--- react/class-notes/RC-09-Appointment/src/components/PatientList.jsx
+++ react/class-notes/RC-09-Appointment/src/components/PatientList.jsx
@@ -4,15 +4,23 @@ const PatientList = ({ hastalar, setHastalar }) => {
   return (
     <div>
       {hastalar.map((patient) => (
-        <div className={patient.isDone ? "trueStil" : "falseStyle"} >
+        <div
+          className={patient.isDone ? "trueStil" : "falseStyle"}
+          onDoubleClick={() =>
+            setHastalar(
+              hastalar.map((hst) =>
+                hst.id === patient.id ? { ...patient, isDone: !hst.isDone } : hst
+              )
+            )
+          }
+        >
           <div>
             <h2>{patient.patientName} </h2>
             <h4>{patient.day} </h4>
             <h3>{patient.myDoctor} </h3>
           </div>
 
-<FaTimesCircle style={{color:"red"}}/>
-       
+          <FaTimesCircle onClick={()=>setHastalar(hastalar.filter((a)=>a.id!==patient.id ))} style={{ color: "red" }} />
         </div>
       ))}
     </div>
