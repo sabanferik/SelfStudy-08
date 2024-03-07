295179a068f35d38f373e45a408ee476f9eb21a5
sabanferik <sbnftmaf@gmail.com>
2024-03-07 09:12:27 +0100

    id  lerle text ile patientName change

--- react/class-notes/RC-09-Appointment/src/helper/Data.jsx
+++ react/class-notes/RC-09-Appointment/src/helper/Data.jsx
@@ -45,23 +45,23 @@ export const hastaData = [
 
 export const doctorData = [
   {
-    id: 0,
+    id: 1,
     doctorName: "DR Ahmet Bilen",
     doctorImg: "https://i.cnnturk.com/i/cnnturk/75/0x555/54857220f97adb1aa472e5e6",
   },
   {
-    id: 1,
+    id: 2,
     doctorName: "DR Ayse Okur",
     doctorImg: "https://i3.posta.com.tr/i/posta/75/750x0/616f757845d2a0b25401f0e1",
   },
   {
-    id: 2,
+    id: 3,
     doctorName: "DR Fatma Adil",
     doctorImg:
       "https://i4.hurimg.com/i/hurriyet/75/0x0/5efd779645d2a04258b8f1cc.jpg",
   },
   {
-    id: 3,
+    id: 4,
     doctorName: "DR Oya Başar",
     doctorImg:
       "https://im.haberturk.com/2019/09/12/2521591_720f96f71c734286b9c93122b8bbd70c_640x640.jpg",
