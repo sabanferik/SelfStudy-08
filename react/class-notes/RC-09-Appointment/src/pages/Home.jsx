abada07fc6f00e74e09d51b249acb98926781322
sabanferik <sbnftmaf@gmail.com>
2024-03-07 09:44:56 +0100

    Dr list okk

--- react/class-notes/RC-09-Appointment/src/pages/Home.jsx
+++ react/class-notes/RC-09-Appointment/src/pages/Home.jsx
@@ -1,16 +1,40 @@
 import React, { useState } from "react";
+import { hastaData, doctorData } from "../helper/Data";
+import PatientList from "../components/PatientList";
 
 const Home = () => {
- 
-
-
-
-
+  const [doctors, setDoctors] = useState(doctorData);
+  const [hastalar, setHastalar] = useState(hastaData);
   return (
-    <div >
-   
-home
-   
+    <div>
+      <div>
+        <header>
+          <h1>HOSPITAL</h1>
+          <div className="dr">
+            {doctors.map((dr) => (
+              <div key={dr.id} >
+                <img
+                  src={dr.doctorImg}
+                  width="180px"
+                  height="150px"
+                  className="btn"
+                  style={{ background: "aqua" }}
+                  alt=""
+                />
+                <h4
+                  style={{ background: "aqua", borderLeft: "10px solid blue" }}
+                >
+                  {dr.doctorName}{" "}
+                </h4>
+              </div>
+            ))}
+          </div>
+        </header>
+
+        {/* hastaekle */}
+      </div>
+
+      <PatientList/>
     </div>
   );
 };
