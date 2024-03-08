294424a0abd3de74cf15b227e3909af0461b40c5
sabanferik <sbnftmaf@gmail.com>
2024-03-08 10:02:36 +0100

    Life end, Clock start

--- react/class-notes/RC-10-lifeCycle/src/App.js
+++ react/class-notes/RC-10-lifeCycle/src/App.js
@@ -3,14 +3,19 @@ import "./App.css";
 import React, { useState } from "react";
 import LifeCycleMethods from "./components/LifeCycleMethods";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Clock from "./components/Clock";
 function App() {
 
   const[show,setShow]=useState(true)
   return (
     <div className="container text-center mt-4">
-      <button className="btn btn-danger" onClick={() => setShow(!show)}>SHOW</button>
+      <button className="btn btn-danger" onClick={() => setShow(!show)}>
+        SHOW
+      </button>
 
-      {show && <LifeCycleMethods />}
+      {/* {show && <LifeCycleMethods />} */}
+
+      {show && <Clock />}
     </div>
   );
 }
--- react/class-notes/RC-10-lifeCycle/src/components/Clock.jsx
+++ react/class-notes/RC-10-lifeCycle/src/components/Clock.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
 const Clock = () => {
-  return (
-    <div>Clock</div>
+ 
+    return (
+    <div>
+<div>
+    <h2>****************************</h2>
+    <h1>
+
+    </h1>
+    <button className='btn btn-warning'>ARTTIR</button>
+</div>
+
+
+    </div>
   )
 }
 
