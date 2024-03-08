80e2bac87376e929e19438a081e48b06d15fe553
sabanferik <sbnftmaf@gmail.com>
2024-03-08 12:37:33 +0100

    Update

--- react/class-notes/RC-08-ToDo-App/src/components/GorevEkle.jsx
+++ react/class-notes/RC-08-ToDo-App/src/components/GorevEkle.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faAnchor} from 'fortawesome/free-solid-svg-icons'
 
 const GorevEkle = ({todos,setTodos}) => {
  const[texT,setText]=useState("")
@@ -31,7 +29,6 @@ setTodos(JSON.parse(localStorage.getItem("gorevler")))
   return (
     <div>
       <header className="header">
-      <FontAwesomeIcon icon={faAnchor} />
         <h1>TO DO APP</h1>
         <button
           className="btn"
