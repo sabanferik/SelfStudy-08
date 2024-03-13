07e8a53068f00d059dee502bc13425e71eb8fc3b
sabanferik <sbnftmaf@gmail.com>
2024-03-13 10:03:11 +0100

    router

--- react/class-notes/RC-12-13-react-router/src/App.js
+++ react/class-notes/RC-12-13-react-router/src/App.js
@@ -5,17 +5,28 @@ import MyNavbar from './components/MyNavbar';
 import Footer from './components/Footer';
 import Teacher from './pages/Teacher';
 import CourseCard from './pages/CourseCard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 const App = () => {
   return (
     <div>
-    <MyNavbar/>
+      <BrowserRouter>
+        <MyNavbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+
+      {/* <MyNavbar/>
       <Home/>
 <Teacher/>
 <CourseCard/>
 
-      <Footer/>
+      <Footer/> */}
     </div>
-  )
+  );
 }
 
 export default App
