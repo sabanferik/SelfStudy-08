3422b984427df4e250d9427c5b934cef32f987d2
sabanferik <sbnftmaf@gmail.com>
2024-03-19 10:06:15 +0100

    product mocApi

--- react/class-notes/RC-14-Shopping-Card/src/pages/ProductList.jsx
+++ react/class-notes/RC-14-Shopping-Card/src/pages/ProductList.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ProductCard from "../components/ProductCard";
 
-
+import axios from "axios"
 const ProductList = () => {
   
-
+ const BASE_URL = "https://63f4e5583f99f5855db9e941.mockapi.io/products";
  
 
  
-
+ const getData=async()=>{
+ const {data}= await axios("https://63f4e5583f99f5855db9e941.mockapi.io/products");
+console.log(data);
+ }
+getData()
   return (
     <div className="container mt-3">
       <div className="d-sm-block d-md-flex">
