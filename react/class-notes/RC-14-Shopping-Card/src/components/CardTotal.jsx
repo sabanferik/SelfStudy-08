503641d2ea30efbcd5fae3a5a3db33367a14313b
sabanferik <sbnftmaf@gmail.com>
2024-03-20 09:22:19 +0100

    Shopping Card second Day End with Subtotal

--- react/class-notes/RC-14-Shopping-Card/src/components/CardTotal.jsx
+++ react/class-notes/RC-14-Shopping-Card/src/components/CardTotal.jsx
@@ -3,29 +3,39 @@ import React from "react";
 const taxRate = 0.18;
 const shipping = 25;
 
-const CardTotal = () => {
-  
-  
-  
+const CardTotal = ({ products }) => {
+  // console.log(products);
+  const subTotal = products.reduce(
+    (acc, product) =>
+      product.price * product.amount * product.dampingRate + acc,
+    0
+  );
+
+  // let subTotal = 0;
+  // products.forEach(
+  //   (product) =>
+  //     (subTotal += product.price * product.amount * product.dampingRate)
+  // );
+
   return (
     <table className="table w-100">
       <tbody>
         <tr className="text-end">
           <th className="text-start">Subtotal</th>
           <td>
-            $<span className="subtotal"></span>
+            $<span className="subtotal">{subTotal.toFixed(2)}</span>
           </td>
         </tr>
         <tr className="text-end">
           <th className="text-start">Tax(18%)</th>
           <td>
-            $<span className="tax"></span>
+            $<span className="tax">{(subTotal * taxRate).toFixed(2)}</span>
           </td>
         </tr>
         <tr className="text-end">
           <th className="text-start">Shipping</th>
           <td>
-            $<span className="shipping"></span>
+            $<span className="shipping">{shipping} </span>
           </td>
         </tr>
         <tr className="text-end">
@@ -33,6 +43,7 @@ const CardTotal = () => {
           <td>
             $
             <span className="total">
+              {(subTotal + shipping + taxRate * subTotal).toFixed(2)}
             </span>
           </td>
         </tr>
--- react/class-notes/RC-14-Shopping-Card/src/pages/ProductList.jsx
+++ react/class-notes/RC-14-Shopping-Card/src/pages/ProductList.jsx
@@ -1,40 +1,34 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 
-import axios from "axios"
+import axios from "axios";
+import CardTotal from "../components/CardTotal";
 const ProductList = () => {
-  
- const BASE_URL = "https://63f4e5583f99f5855db9e941.mockapi.io/products";
- 
-const [produkte,setProdukte]=useState([])
-const [error,setError]=useState(false)
-const [loading,setLoading]=useState(true)
- 
- const getData=async()=>{
-  try {
-    const { data } = await axios(
-      BASE_URL
-    );
-    setProdukte(data)
-  } catch (e) {
-    setError(true)
-    console.log(e);
-  }finally{
-    setLoading(false)
+  const BASE_URL = "https://63f4e5583f99f5855db9e941.mockapi.io/products";
+
+  const [produkte, setProdukte] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const getData = async () => {
+    try {
+      const { data } = await axios(BASE_URL);
+      setProdukte(data);
+    } catch (e) {
+      setError(true);
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (error) {
+    return <p>Something went wrong.....</p>;
   }
- 
-
- }
-
-useEffect(()=>{getData()},[])
-
-
-
-
-
-if(error){
-  return <p>Something went wrong.....</p>
-}
 
   return (
     <div className="container mt-3">
@@ -45,9 +39,15 @@ if(error){
           <>
             <article id="product-panel" className="col-md-6">
               {produkte.map((product) => (
-                
-                <ProductCard key={product.id} product={product} getData={getData}/>
+                <ProductCard
+                  key={product.id}
+                  product={product}
+                  getData={getData}
+                />
               ))}
+            </article>
+            <article>
+              <CardTotal products={produkte} />
             </article>
           </>
         )}
