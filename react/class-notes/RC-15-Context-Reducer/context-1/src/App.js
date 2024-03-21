55558852e9aeba0c54f086d2f93e7998c1399f8c
sabanferik <sbnftmaf@gmail.com>
2024-03-21 10:12:39 +0100

    with color change erste lesson end

--- react/class-notes/RC-15-Context-Reducer/context-1/src/App.js
+++ react/class-notes/RC-15-Context-Reducer/context-1/src/App.js
@@ -5,12 +5,18 @@ import Home from "./components/Home";
 //!1- context alanı açıyoruz
 export const StudentContext=createContext()
 
-const App = () => {
 
+const App = () => {
+const [students,setStudents]=useState(data)
+
+const changeColor=(id,newRenk)=>{
+setStudents(students.map((a)=>a.id===id ? {...a,color:newRenk}:a))
+
+}
 
   return (
     //!2- bütün projeye gönderilmek üzere ilk Home u, gönderilecek elemanlarla sarmallayalım
-    <StudentContext.Provider value={{}}>
+    <StudentContext.Provider value={{students,changeColor}}>
       <Home />
     </StudentContext.Provider>
   );
--- react/class-notes/RC-15-Context-Reducer/context-1/src/components/OgrenciItem.jsx
+++ react/class-notes/RC-15-Context-Reducer/context-1/src/components/OgrenciItem.jsx
@@ -1,16 +1,22 @@
+import { useContext } from "react"
+import { StudentContext } from "../App"
 
 
 const OgrenciItem = () => {
+    //!3- (context consumer) globalde tanımladığım değerleri buraya çağırdık
+const{students,changeColor}=useContext(StudentContext)
+console.log(students);
 
   return (
     <div>
-{[].map((i)=>{
+{students.map((i)=>{
 return(
-    <div style={{backgroundColor:i.color}}>
+    <div style={{backgroundColor:i.color}} key={i.id}>
         <h3>NAME: {i.name}</h3>
         <h4>EMAİL: {i.email} </h4>
         <h4> AGE: {i.age}</h4>
-        Color: <input type="text"  
+        Color: <input type="text"  value={i.color}
+          onChange={(e)=>changeColor(i.id,e.target.value)}
           
         />
     </div>
