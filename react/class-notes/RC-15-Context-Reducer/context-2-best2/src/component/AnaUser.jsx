bcb5871b5a2196c9b0d5d0e8f525198d212b7351
sabanferik <sbnftmaf@gmail.com>
2024-03-21 12:06:56 +0100

    context-2-best2  end

--- react/class-notes/RC-15-Context-Reducer/context-2-best2/src/component/AnaUser.jsx
+++ react/class-notes/RC-15-Context-Reducer/context-2-best2/src/component/AnaUser.jsx
@@ -1,26 +1,28 @@
 import React from 'react'
+import { useKullaniciCntx } from '../context/KullaniciProvider'
 
 
 
-const AnaUser = () => {
-
+const AnaUser = ({users}) => {
+const{changeWidth}=useKullaniciCntx()
     
   return (
     <div>
-{[].map((i)=>{
-    return(<div>
-
+{users.map((i)=>{
+    return (
+      <div>
         <h3>{i.login} </h3>
-        <img src={i.avatar_url} alt="" width={i.width}/>
+        <img src={i.avatar_url} alt="" width={i.width} />
 
         <div>
-            <label htmlFor="">Image width(px)</label>
-            <input type="number" 
-               
-            />
+          <label htmlFor="">Image width(px)</label>
+          <input
+            type="number"
+            onChange={(e) => changeWidth(i.id, e.target.value)}
+          />
         </div>
-
-    </div>)
+      </div>
+    );
 })}
 
     </div>
--- react/class-notes/RC-15-Context-Reducer/context-2-best2/src/context/KullaniciProvider.jsx
+++ react/class-notes/RC-15-Context-Reducer/context-2-best2/src/context/KullaniciProvider.jsx
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
+import { createContext } from "react";
 
 
 import { useEffect } from "react";
 import { useState } from "react";
 
+//!1- Create Context
 
+export const KullaniciContext=createContext();
 
 //!Provider
-const KullaniciProvider = () => {
+const KullaniciProvider = ({children}) => {
 
+const [users, setUsers] = useState([]);
 
+useEffect(() => {
+  fetch("https://api.github.com/users")
+    .then((res) => res.json())
+    .then((data) => setUsers(data));
+}, []);
+
+const changeWidth = (id, size) => {
+  setUsers(users.map((a) => (a.id === id ? { ...a, width: size } : a)));
+};
 
 
   return (
-   <div></div>
+   <KullaniciContext.Provider value={{users,changeWidth}}>
+{children}
+   </KullaniciContext.Provider>
   );
 };
+
+
+//!Consuming burada yaparsak ihtiyaç duyan child larda bir daha çağırmıyoruz (opsiyonel tercih etmeyebilir, eski yoldan yani verileri çağıran sayfalara useContext yazarak devam edebilirsiniz.)
+
+export const useKullaniciCntx=()=>{
+
+  return useContext(KullaniciContext);
+}
+
 
 
 
