fcffeebef658032c6de89fe6d4a6aff5b8b34c1f
sabanferik <sbnftmaf@gmail.com>
2024-03-22 09:46:24 +0100

    styledComponents-intro first und styled

---
+++ react/class-notes/RC-16-styledComponents-intro/src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import HeaderS, { LinkS } from './style/HeaderStyle'
+
+const Home = () => {
+  return (
+    <div>
+     
+     <HeaderS>STYLED COMPONENTS</HeaderS>
+
+     <LinkS href="#">LINK</LinkS>
+     
+
+
+     <button>TIKLA1</button>
+     <button>TIKLA2</button>
+     <button>TIKLA3</button>
+     <button>TIKLA4</button>
+     <button>TIKLA5</button>
+    </div>
+  )
+}
+
+export default Home
