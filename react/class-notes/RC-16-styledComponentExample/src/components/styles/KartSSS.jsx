ca3e446af07b31aeb955ddfbeab80e2d21a50c9c
sabanferik <sbnftmaf@gmail.com>
2024-03-22 12:05:26 +0100

    Cards und responsive

--- react/class-notes/RC-16-styledComponentExample/src/components/styles/KartSSS.jsx
+++ react/class-notes/RC-16-styledComponentExample/src/components/styles/KartSSS.jsx
@@ -1,4 +1,17 @@
 
+import styled from 'styled-components';
 
+const KartSSS = styled.div`
+  background-color: aquamarine;
+
+  border-radius: 15px;
+  margin-bottom: 2rem;
+  gap: 2rem;
+  padding: 60px;
+  display: flex;
+  flex-direction: ${({nida})=>nida };
+`;
+
+export default KartSSS;
 
 
