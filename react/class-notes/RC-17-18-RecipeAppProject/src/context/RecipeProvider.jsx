d8c13d65e202f44d51ba84c695ef7a92c7574d2d
sabanferik <sbnftmaf@gmail.com>
2024-03-27 12:11:04 +0100

    receip end

--- react/class-notes/RC-17-18-RecipeAppProject/src/context/RecipeProvider.jsx
+++ react/class-notes/RC-17-18-RecipeAppProject/src/context/RecipeProvider.jsx
@@ -16,6 +16,8 @@ const RecipeProvider = ({ children }) => {
 
   //!home, header ve recipeCard sayfaları için
   const [recipes, setRecipes] = useState([]);
+  const[loading,setLoading]=useState(false)
+  const [error,setError]=useState(false)
   const [query, setQuery] = useState("");
   const [mealType, setMealType] = useState("Breakfast");
 console.log(mealType);
@@ -23,13 +25,26 @@ const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=$
 
 
 const getData=async()=>{
+  setLoading(true)
+  
+try {
+  const { data } = await axios.get(url);
+  // console.log(data.hits);
+  setRecipes(data.hits);
 
- const {data}=  await axios.get(url)
-console.log(data.hits);
-setRecipes(data.hits)
+} catch (error) {
+  setError(true)
+  
+}finally {
+  setLoading(false)
 }
-
-
+ 
+}
+if (error) {
+  return <p>Something went wrong.....</p>;
+}
+if(loading)
+{return <p>loading...</p>}
 
   return (
     <RecipeContext.Provider
@@ -50,3 +65,47 @@ setRecipes(data.hits)
 };
 
 export default RecipeProvider;
+
+
+
+// //! Reducer Lı Kullanım
+// import axios from "axios";
+// import React, { createContext, useContext, useEffect, useReducer } from "react";
+
+// // statelerin oluşturulması
+// const state = {
+//   recipes: [],
+//   loading: false,
+//   error: false,
+// };
+
+// // Reducer fonksiyonu: State'i güncelleyen actionlar
+// const reducer = (state, { type, payload }) => {
+//   switch (type) {
+//     case "RECIPES":
+//       return { ...state, recipes: payload };
+//     case "LOADING":
+//       return { ...state, loading: payload };
+//     case "ERROR":
+//       return { ...state, error: payload };
+//     default:
+//       return state;
+//   }
+// };
+
+//   // State değerlerinin ayrıştırılması
+//   const { recipes, loading, error } = state;
+
+//   const getData = async () => {
+//     dispatch({ type: "LOADING", payload: true }); // loading state ini güncelledik
+//     try {
+//       const { data } = await axios(
+//         url
+//       );
+//       dispatch({ type: "RECIPES", payload: data.hits }); // gelen ürünler i dispatch ile state imize aktarıyoruz.
+//     } catch (error) {
+//        dispatch({ type: "ERROR", payload: true });
+//     } finally {
+//       dispatch({ type: "LOADING", payload: false }); // laoding stateini tekrar false a çekiyoruz
+//     }
+//   };
