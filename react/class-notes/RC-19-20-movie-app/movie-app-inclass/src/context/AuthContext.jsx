e6e607bd665c6d80db97b285ff577ff86516f4db
sabanferik <sbnftmaf@gmail.com>
2024-04-02 12:10:12 +0200

    userObserver

--- react/class-notes/RC-19-20-movie-app/movie-app-inclass/src/context/AuthContext.jsx
+++ react/class-notes/RC-19-20-movie-app/movie-app-inclass/src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { createContext, useContext, useState } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged, updateProfile } from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
 import { auth } from "../auth/firebase";
 
 //! create context
@@ -10,14 +10,36 @@ const AuthContext = createContext();
 const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState("");
 
-  const register = async (email, password) => {
+  const register = async (email, password,displayName) => {
     const userCredential = await createUserWithEmailAndPassword(
       auth,
       email,
       password
     );
+    await updateProfile(auth.currentUser, {
+        displayName: displayName
+      })
     console.log(userCredential);
   };
+
+  const userObserver = () => {
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/auth.user
+          const {email,displayName,photoURL} = user;
+          setCurrentUser({email,displayName,photoURL})
+        } else {
+          // User is signed out
+          // ...
+          setCurrentUser(false)
+        }
+      });
+  }
+  console.log(currentUser)
+  useEffect(()=>{
+    userObserver()
+  },[])
 
   return (
     <AuthContext.Provider value={{ currentUser,register }}>
