47d1a59bc9bfe2ed6b046beabaf7c2f8e683056f
sabanferik <sbnftmaf@gmail.com>
2024-04-02 11:16:22 +0200

    firebase config

---
+++ react/class-notes/RC-19-20-movie-app/movie-app-inclass/src/context/AuthContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from "react";
+
+//! create context
+const AuthContext = createContext();
+
+//? context provider
+
+const AuthContextProvider = ({children}) => {
+    const [currentUser,setCurrentUser] = useState("")
+
+    return <AuthContext.Provider value={{currentUser}}>{children}</AuthContext.Provider>
+};
+
+//* consumer with custom hook
+export const useAuthContext = () => {
+    return useContext(AuthContext)
+}
+
+export default AuthContextProvider;
