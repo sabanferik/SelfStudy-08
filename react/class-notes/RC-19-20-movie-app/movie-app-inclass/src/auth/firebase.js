5a0b7605726b4cd78dbd11f9f23566a48b452e11
sabanferik <sbnftmaf@gmail.com>
2024-04-02 12:25:20 +0200

    loginupdate

--- react/class-notes/RC-19-20-movie-app/movie-app-inclass/src/auth/firebase.js
+++ react/class-notes/RC-19-20-movie-app/movie-app-inclass/src/auth/firebase.js
@@ -3,22 +3,22 @@ import { getAuth } from "firebase/auth";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDtcdq-Efl6CxzxAUCZvu85wdnzO4R0Vb0",
-//     authDomain: "movie-app-de8.firebaseapp.com",
-//     projectId: "movie-app-de8",
-//     storageBucket: "movie-app-de8.appspot.com",
-//     messagingSenderId: "1097072051272",
-//     appId: "1:1097072051272:web:40ec5e225ea3526abbc47d"
-// };
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId
+    apiKey: "AIzaSyDtcdq-Efl6CxzxAUCZvu85wdnzO4R0Vb0",
+    authDomain: "movie-app-de8.firebaseapp.com",
+    projectId: "movie-app-de8",
+    storageBucket: "movie-app-de8.appspot.com",
+    messagingSenderId: "1097072051272",
+    appId: "1:1097072051272:web:40ec5e225ea3526abbc47d"
 };
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_apiKey,
+//     authDomain: process.env.REACT_APP_authDomain,
+//     projectId: process.env.REACT_APP_projectId,
+//     storageBucket: process.env.REACT_APP_storageBucket,
+//     messagingSenderId: process.env.REACT_APP_messagingSenderId,
+//     appId: process.env.REACT_APP_appId
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
