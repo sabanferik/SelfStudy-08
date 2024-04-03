0ef990688852ac4a8b90914d0db0e58bf2af00b9
sabanferik <sbnftmaf@gmail.com>
2024-04-03 10:53:36 +0200

    movie context

---
+++ react/class-notes/RC-19-20-movie-app/movie-app-inclass/src/context/MovieContext.jsx
@@ -0,0 +1,14 @@
+import { createContext, useContext } from "react";
+
+const MovieContext = createContext();
+
+const MovieContextProvider = ({children}) => {
+
+    return <MovieContext.Provider value={}>{children}</MovieContext.Provider>
+}
+
+export const useMovieContext = () => {
+    return useContext(MovieContext)
+}
+
+export default MovieContextProvider;
