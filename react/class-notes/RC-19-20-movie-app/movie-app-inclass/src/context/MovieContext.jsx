18d0bab0e9eea68cf035c241e1ed7dd7697faddf
sabanferik <sbnftmaf@gmail.com>
2024-04-03 11:13:37 +0200

    movie context data

--- react/class-notes/RC-19-20-movie-app/movie-app-inclass/src/context/MovieContext.jsx
+++ react/class-notes/RC-19-20-movie-app/movie-app-inclass/src/context/MovieContext.jsx
@@ -1,10 +1,33 @@
-import { createContext, useContext } from "react";
-
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios"
 const MovieContext = createContext();
 
-const MovieContextProvider = ({children}) => {
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`
 
-    return <MovieContext.Provider value={}>{children}</MovieContext.Provider>
+const MovieContextProvider = ({children}) => {
+    const [movies,setMovies] = useState([]);
+    const [loading,setLoading] = useState(false);
+
+    const getMovies = async (apiUrl) => {
+        setLoading(true);
+        try {
+            const {data} = await axios(apiUrl)
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(()=>{
+        getMovies(url);
+    },[])
+
+    const values= {getMovies,movies,loading}
+
+    return <MovieContext.Provider value={values}>{children}</MovieContext.Provider>
 }
 
 export const useMovieContext = () => {
