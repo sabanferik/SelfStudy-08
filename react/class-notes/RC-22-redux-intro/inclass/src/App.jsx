c6699c6eacaabbc4629edc238887b8c9d6a24c4a
sabanferik <sbnftmaf@gmail.com>
2024-04-08 12:25:36 +0200

    todo import

--- react/class-notes/RC-22-redux-intro/inclass/src/App.jsx
+++ react/class-notes/RC-22-redux-intro/inclass/src/App.jsx
@@ -3,14 +3,15 @@ import "./App.css";
 import Counter from "./components/counter/Counter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { store } from "./store/store";
+import Todo from "./components/todo/Todo";
 
 function App() {
   return (
     <div className="app">
       <Provider store={store}>
         <ErrorBoundary>
-          <Counter />
-          {/* <Todo /> */}
+          {/* <Counter /> */}
+          <Todo />
         </ErrorBoundary>
       </Provider>
     </div>
