a3e90e8376968430ef0b5322111edd1d9ac48488
sabanferik <sbnftmaf@gmail.com>
2024-04-09 12:31:47 +0200

    products

--- react/class-notes/RC-23-redux-toolkit-projects/inclass/src/features/productSlice.js
+++ react/class-notes/RC-23-redux-toolkit-projects/inclass/src/features/productSlice.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   productsData: [],
@@ -16,7 +17,8 @@ const initialState = {
 // action type’larını oluşturur.
 
 export const getProductsData = createAsyncThunk("getProducts", async () => {
-  const { data } = await axios("https://dummyjson.com/products");
+  const { data } = await axios("https://dummyjson.com/productsasdasdasdasd");
+  console.log(data);
   return data.products;
 });
 
@@ -24,9 +26,9 @@ const productSlice = createSlice({
   name: "productsApi",
   initialState,
   reducers: {
-    clearProductsData : (state)=>{
-        state.productsData = []
-    }
+    clearProductsData: (state) => {
+      state.productsData = [];
+    },
   },
   extraReducers: (builder) => {
     builder
@@ -45,11 +47,9 @@ const productSlice = createSlice({
   },
 });
 
-export const {clearProductsData} = productSlice.actions
+export const { clearProductsData } = productSlice.actions;
 
 export default productSlice.reducer;
-
-
 
 //! createAsyncThunk
 // Asenkron İşlemlerin Kolay Yönetimi: Redux'ta, asenkron işlemleri yönetmek için birçok ekstra kod yazmanız gerekir. createAsyncThunk, asenkron işlemleri tanımlamayı ve bu işlemlerin durumlarını (pending, fulfilled, rejected) otomatik olarak yönetmeyi sağlar. Bu sayede, kodunuz daha okunabilir ve yönetilebilir hale gelir.
@@ -60,8 +60,7 @@ export default productSlice.reducer;
 
 // İşlevsellik ve Performans: createAsyncThunk, asenkron işlemleri yönetmek için Redux Middleware'lerini kullanır. Bu Middleware'ler, işlevsellik ve performans açısından optimize edilmiştir, böylece asenkron işlemler sorunsuz bir şekilde çalışır.
 
-
-//! Veri çekme işlemlerini neden reducerlarda yapamıyoruz da middlewarelere ihtiyaç duyuyoruz ? 
+//! Veri çekme işlemlerini neden reducerlarda yapamıyoruz da middlewarelere ihtiyaç duyuyoruz ?
 // Çünkü Reducer’larımız pure olmalı ve asla side effect yaratacak işlemleri barındırmamalıdır. Bazı durumlarda -hatta çoğu durumda- pure olmayan işlemler yapmak zorunda kalırız. Bu işlemler; api istekleri, bazı javascript fonksiyonları(math.random) veya kendi uygulamamızın gerektirdikleri olabilir. İşte tam bu noktada middleware ihtiyacı ortaya çıkıyor. Tabii, bu middleware’ler sadece side effect işlemleri için yoklar. Aynı zamanda state’timizi takip etmek için bazı ekstra araçları da middleware olarak kullanabiliriz.
 // ReduxToolikt içerisinde api istekleri için kullandığımız middleware de createAsyncThunk olmuş oluyor.
 // Pure Redux içerisinde bunu doğrudan redux-thunk kullanarak yapıyoruz.
