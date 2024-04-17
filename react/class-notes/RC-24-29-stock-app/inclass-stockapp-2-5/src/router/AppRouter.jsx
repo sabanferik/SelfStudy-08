f28898a8dbe66a97b778994f8e87ff46708ae7b0
sabanferik <sbnftmaf@gmail.com>
2024-04-17 10:51:56 +0200

    new pages

--- react/class-notes/RC-24-29-stock-app/inclass-stockapp-2-5/src/router/AppRouter.jsx
+++ react/class-notes/RC-24-29-stock-app/inclass-stockapp-2-5/src/router/AppRouter.jsx
@@ -1,7 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// import LoginMid from "../pages/LoginMid";
-// import RegisterMid from "../pages/RegisterMid";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import PrivateRouter from "./PrivateRouter";
@@ -11,9 +8,6 @@ const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        {/* middlewareli fonksiyonların olduğu sayfalar */}
-        {/* <Route path="/" element={<LoginMid />} />
-        <Route path="register" element={<RegisterMid />} /> */}
         <Route path="/" element={<Login />} />
         <Route path="register" element={<Register />} />        
         <Route path="stock" element={<PrivateRouter />}>
