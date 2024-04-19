cae43d389951ee43bf77d57462f75bd0d4255540
sabanferik <sbnftmaf@gmail.com>
2024-04-19 09:50:25 +0200

    Firms.jsx

--- react/class-notes/RC-24-29-stock-app/inclass-stockapp-2-5/src/pages/Firms.jsx
+++ react/class-notes/RC-24-29-stock-app/inclass-stockapp-2-5/src/pages/Firms.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {useDispatch, useSelector} from "react-redux";
+import { fetchFail, fetchStart, firmsSuccess } from '../features/stockSlice';
+import axios from "axios";
 
 const Firms = () => {
+  const dispatch = useDispatch()
+  const {token} = useSelector(state => state.auth)
+
+  const getFirms = async () => {
+    dispatch(fetchStart())
+    try {
+      const {data} = await axios(`${import.meta.env.VITE_BASE_URL}firms`,{
+        headers:{
+          Authorization: `Token ${token}`
+          // Authorization: `Bearer ${accesstoken}` //* jwt için
+        }
+      })
+      console.log(data)
+      dispatch(firmsSuccess(data.data))
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail())
+    }
+  }
+
+  useEffect(() => {
+    getFirms()
+  }, [])
+
   return (
     <div>Firms</div>
   )
