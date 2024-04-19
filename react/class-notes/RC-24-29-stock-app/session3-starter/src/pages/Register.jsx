ae7a813d0321bcd9f83d52107a1cf5d54d882f15
sabanferik <sbnftmaf@gmail.com>
2024-04-19 08:45:12 +0200

    than pc reperierd Stock App erste commit

--- react/class-notes/RC-24-29-stock-app/session3-starter/src/pages/Register.jsx
+++ react/class-notes/RC-24-29-stock-app/session3-starter/src/pages/Register.jsx
@@ -1,25 +1,26 @@
-import Avatar from "@mui/material/Avatar";
-import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
 import LockIcon from "@mui/icons-material/Lock";
-import image from "../assets/hero.png";
+import image from "../assets/regi.avif";
+import Grid from "@mui/material/Grid";
 import { Link } from "react-router-dom";
+import { Box } from "@mui/material";
 import AuthHeader from "../components/AuthHeader";
 import AuthImage from "../components/AuthImage";
-import { Formik } from "formik";
+import { Formik} from "formik";
 import useAuthCall from "../hooks/useAuthCall";
-import LoginForm, { loginScheme } from "../components/LoginForm";
+import RegisterForm, { SignupSchema } from "../components/RegisterForm";
 
-const Login = () => {
-  const { login } = useAuthCall();
+const Register = () => {
+  const {register} = useAuthCall()
   return (
     <Container maxWidth="lg">
       <Grid
         container
         justifyContent="center"
         direction="row-reverse"
+        rowSpacing={{ sm: 3 }}
         sx={{
           height: "100vh",
           p: 2,
@@ -30,7 +31,7 @@ const Login = () => {
         <Grid item xs={12} sm={10} md={6}>
           <Avatar
             sx={{
-              backgroundColor: "secondary.main",
+              backgroundColor: "secondary.light",
               m: "auto",
               width: 40,
               height: 40,
@@ -38,25 +39,33 @@ const Login = () => {
           >
             <LockIcon size="30" />
           </Avatar>
-          <Typography variant="h4" align="center" mb={4} color="secondary.main">
-            SIGN IN
+          <Typography
+            variant="h4"
+            align="center"
+            mb={2}
+            color="secondary.light"
+          >
+            Register
           </Typography>
 
           <Formik
-            initialValues={{ email: "", password: "" }}
-            validationSchema={loginScheme}
+            initialValues={{
+              username: "",
+              firstName: "",
+              lastName: "",
+              email: "",
+              password: "",
+            }}
+            validationSchema={SignupSchema}
             onSubmit={(values, actions) => {
-              login(values);
+              register(values);
               actions.resetForm();
               actions.setSubmitting(false);
             }}
-            component={props => <LoginForm {...props} />}>
-              
-            </Formik>
+            component={(props) => <RegisterForm {...props} />}
+          ></Formik>
           <Box sx={{ textAlign: "center", mt: 2, color: "secondary.main" }}>
-            <Link to="/register">
-              Don't have an account? Sign Up
-            </Link>
+            <Link to="/">Already have an account? Sign in</Link>
           </Box>
         </Grid>
 
@@ -66,4 +75,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
