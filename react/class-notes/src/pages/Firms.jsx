eb578151ef761a46e769657eaa523e9713bc6596
sabanferik <sbnftmaf@gmail.com>
2024-04-21 21:28:40 +0200

    Delete project files
    This commit deletes several files related to a React project, including the main entry point, various pages, components, hooks, and a router. The specific files deleted are:
    - App.jsx
    - app/

--- react/class-notes/src/pages/Firms.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Firms = () => {
-  return (
-    <div>Firms</div>
-  )
-}
-
-export default Firms
--- react/class-notes/src/pages/Purchases.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Purchases = () => {
-  return (
-    <div>Purchases</div>
-  )
-}
-
-export default Purchases
