025fe2115e0f2201b7a0761bcbb14966fe55fc44
sabanferik <sbnftmaf@gmail.com>
2024-04-22 09:51:51 +0200

    axios intsance

--- react/class-notes/RC-24-29-stock-app/inclass-stockapp-2-5/src/hooks/useAxios.jsx
+++ react/class-notes/RC-24-29-stock-app/inclass-stockapp-2-5/src/hooks/useAxios.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import axios from "axios";
+import { useSelector } from "react-redux";
+
+export const axiosWithPublic = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+});
 
 const useAxios = () => {
-  return (
-    <div>useAxios</div>
-  )
-}
+  const { token } = useSelector((state) => state.auth);
+  const axiosWithToken = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL,
+    headers: { Authorization: `Token ${token}` },
+  });
+  return axiosWithToken;
+};
 
-export default useAxios
+export default useAxios;
