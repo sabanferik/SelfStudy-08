78b4976853a9ce04c72625da04c34cea6ad7c5c6
sabanferik <sbnftmaf@gmail.com>
2024-04-22 09:58:29 +0200

    "Replace direct axios calls with useAxios hook in useStockCall"
    This commit updates the `useStockCall` hook to use the `useAxios` hook for making API calls, instead of directly using

--- react/class-notes/RC-24-29-stock-app/inclass-stockapp-2-5/src/hooks/useStockCall.jsx
+++ react/class-notes/RC-24-29-stock-app/inclass-stockapp-2-5/src/hooks/useStockCall.jsx
@@ -7,12 +7,13 @@ import {
   // firmsSuccess,
   getSuccess,
 } from "../features/stockSlice";
+import useAxios from "./useAxios";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 const useStockCall = () => {
   const dispatch = useDispatch();
   const { token } = useSelector((state) => state.auth);
-
+  const axiosWithToken = useAxios()
   // const getFirms = async () => {
   //   dispatch(fetchStart());
   //   try {
@@ -53,12 +54,13 @@ const useStockCall = () => {
   const getStockData = async (url) => {
     dispatch(fetchStart());
     try {
-      const { data } = await axios(`${BASE_URL}${url}`, {
-        headers: {
-          Authorization: `Token ${token}`,
-          // Authorization: `Bearer ${accesstoken}` //* jwt için
-        },
-      });
+      // const { data } = await axios(`${BASE_URL}${url}`, {
+      //   headers: {
+      //     Authorization: `Token ${token}`,
+      //     // Authorization: `Bearer ${accesstoken}` //* jwt için
+      //   },
+      // });
+      const { data } = await axiosWithToken(`${url}`);
       console.log(data);
       // dispatch(brandsSuccess(data.data));
       // dispatch(getSuccess({data:data.data,url:url}));//* action creatorlar her zaman tek bir parametre kabul ederler
@@ -68,15 +70,16 @@ const useStockCall = () => {
       dispatch(fetchFail());
     }
   };
-
+ //! istek atarken ortak olan base_url  ve token gibi değerleri her seferinde yazmak yerine axios instance kullanarak bunları orada tanımlıyoruz. Ve sonrasında sadece istek atmak istediğimiz end pointi yazmamız yeterli oluyor.
   const deleteStockData = async (url,id) => {
     dispatch(fetchStart());
     try {
-      await axios.delete(`${BASE_URL}${url}/${id}`, {
-        headers: {
-          Authorization: `Token ${token}`,
-        },
-      });
+      // await axios.delete(`${BASE_URL}${url}/${id}`, {
+      //   headers: {
+      //     Authorization: `Token ${token}`,
+      //   },
+      // });
+      await axiosWithToken.delete(`${url}/${id}`);
       // getStockData(url)
     } catch (error) {
       console.log(error);
