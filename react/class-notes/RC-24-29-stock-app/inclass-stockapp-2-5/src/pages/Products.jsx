be42cd5d5c3040ad14c21202583b1c5449fd779c
sabanferik <sbnftmaf@gmail.com>
2024-04-24 09:22:26 +0200

    "Added ProductModal and useStockCall hooks, updated Products page to include 'New Product' button and modal for adding new products"

--- react/class-notes/RC-24-29-stock-app/inclass-stockapp-2-5/src/pages/Products.jsx
+++ react/class-notes/RC-24-29-stock-app/inclass-stockapp-2-5/src/pages/Products.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import ProductModal from "../components/Modals/ProductModal";
+import useStockCall from "../hooks/useStockCall";
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
+  const { getStockData } = useStockCall();
+  const { products } = useSelector((state) => state.stock);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setInitialState({
+      categoryId: "",
+      brandId: "",
+      name: "",
+    });
+  };
+  const [initialState, setInitialState] = useState({
+    categoryId: "",
+    brandId: "",
+    name: "",
+  });
+  console.log("products:", products);
+  console.log("products:", initialState);
+  useEffect(() => {
+    getStockData("products");
+  }, []);
 
-export default Products
+  return (
+    <Container maxWidth={"xl"}>
+      <Typography
+        align="center"
+        variant="h4"
+        component="h1"
+        color="secondary.second"
+      >
+        Products
+      </Typography>
+      <Button variant="contained" onClick={handleOpen}>
+        New Product
+      </Button>
+      {open && (
+        <ProductModal
+          open={open}
+          handleClose={handleClose}
+          initialState={initialState}
+        />
+      )}
+    </Container>
+  );
+};
+
+export default Products;
