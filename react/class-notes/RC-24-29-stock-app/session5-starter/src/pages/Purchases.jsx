4001c963acf9643bb5073e6d42bd2ba1dd92808e
sabanferik <sbnftmaf@gmail.com>
2024-04-24 09:07:56 +0200

    Update components and pages with new styles and global styles import; add loading gif to Brands page
    Components and pages have been updated with new styles and global styles have been imported from a separate file. Additionally, a loading gif has been

---
+++ react/class-notes/RC-24-29-stock-app/session5-starter/src/pages/Purchases.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+import useStockCall from '../hooks/useStockCall'
+
+const Purchases = () => {
+  const {getStockData} = useStockCall()
+  useEffect(()=>{
+    getStockData("firms")
+  },[])
+  return (
+    <div>Purchases</div>
+  )
+}
+
+export default Purchases
