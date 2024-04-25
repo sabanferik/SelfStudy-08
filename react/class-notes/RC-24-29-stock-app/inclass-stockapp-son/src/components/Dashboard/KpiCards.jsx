6ad7d445221bc743d09a57bcb9019f51635d91a0
sabanferik <sbnftmaf@gmail.com>
2024-04-25 11:34:18 +0200

    "Updated KpiCards component to display sales, cash, and purchases data with corresponding colors; added total sales calculation using Redux state"
    Explanation:
    
    * The KpiCards component has been updated to display new

--- react/class-notes/RC-24-29-stock-app/inclass-stockapp-son/src/components/Dashboard/KpiCards.jsx
+++ react/class-notes/RC-24-29-stock-app/inclass-stockapp-son/src/components/Dashboard/KpiCards.jsx
@@ -1,42 +1,49 @@
 // 'use client';
 import { Card } from '@tremor/react';
-
+import { useSelector } from "react-redux";
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
 const data = [
   {
-    name: 'Recurring revenue',
-    value: '$34.1K',
+    name: 'Sales',
+    value: '€ 34.1K',
     change: '+6.1%',
     changeType: 'positive',
+    color:"indigo"
   },
   {
-    name: 'Total users',
-    value: '500.1K',
+    name: 'Cash',
+    value: '€ 34.5',
     change: '+19.2%',
     changeType: 'positive',
+    color:"amber"
   },
   {
-    name: 'User growth',
-    value: '11.3%',
+    name: 'Purchases',
+    value: '€ 34.5',
     change: '-1.2%',
     changeType: 'negative',
+    color:"fuchsia"
   },
 ];
 
 export default function KpiCards() {
+    const {sales,purchases} = useSelector(state=> state.stock)
+    const totalSales = sales?.reduce((acc,item) => acc + item.amount,0)
+    console.log(totalSales)
   return (
     <>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {data.map((item) => (
-          <Card key={item.name}>
+          <Card key={item.name} decoration="top"
+          decorationColor={item.color}>
             <p className="flex items-start justify-between">
               <span className="text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
                 {item.value}
               </span>
-              <span
+              {/* <span
                 className={classNames(
                   item.changeType === 'positive'
                     ? 'text-emerald-700 dark:text-emerald-500'
@@ -45,7 +52,7 @@ export default function KpiCards() {
                 )}
               >
                 {item.change}
-              </span>
+              </span> */}
             </p>
             <p className="mt-1 text-tremor-default text-tremor-content dark:text-dark-tremor-content">
               {item.name}
