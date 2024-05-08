8232c1e67b675f59c5761a899fe9b6c8a9e3f4fc
sabanferik <sbnftmaf@gmail.com>
2024-05-08 11:52:36 +0200

    Update Home.tsx and AddTodoComp.tsx: added axios for API calls, updated prop types and state management
    In Home.tsx, added useEffect hook to fetch todos from a mock API using axios.

--- typeScript/ts-todo-app/src/pages/Home.tsx
+++ typeScript/ts-todo-app/src/pages/Home.tsx
@@ -1,24 +1,59 @@
 import { Container, Typography } from "@mui/material";
 import AddTodoComp from "../components/AddTodoComp";
 import TodoList from "../components/TodoList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 interface ITodoType {
     task: string ;
     isDone: boolean;
     id: string | number;
 }
+const url = "https://634ac3fc5df952851418480f.mockapi.io/api/todos";
 
 const Home = () =>{
     // const [todos,setTodos] = useState([] as ITodoType[])
     // const [todos,setTodos] = useState<Array<ITodoType>>([])
     const [todos,setTodos] = useState<ITodoType[]>([]) //* yaygın olan kullanım 
+
+    const getTodos = async () => {
+        try {
+            const {data} = await axios<ITodoType[]>(url)
+            setTodos(data)
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    //! function type tanımlama 1.yol
+    // const addTodo = async (text:string) =>{
+    //     try {
+            
+    //     } catch (error) {
+            
+    //     }
+    // }
+    //? function type tanımlama 2.yol
+    // type AddFn = (text:string) => Promise<void>;
+    const addTodo:AddFn = async (text) =>{
+        try {
+            await axios.post(url,{task:text,isDone:false})
+            getTodos()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(()=>{
+      getTodos()
+    },[])
+
     return (
         <Container>
             <Typography align="center" color="error" variant="h2" component="h1">
             Todo App with Typescript
             </Typography>
-            <AddTodoComp />
+            <AddTodoComp addTodo={addTodo} />
             <TodoList/>
         </Container>
     )
