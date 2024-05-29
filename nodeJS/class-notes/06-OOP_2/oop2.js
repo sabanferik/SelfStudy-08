2484af506108f2028c52c245fb42aac68f868dee
sabanferik <sbnftmaf@gmail.com>
2024-05-29 10:52:32 +0200

    "Added and modified classes, constructors, and methods in oop2.js, including Vehicle, Car, and Accessory classes, with changes to constructors, properties, and methods."

--- nodeJS/class-notes/06-OOP_2/oop2.js
+++ nodeJS/class-notes/06-OOP_2/oop2.js
@@ -82,10 +82,11 @@ console.log('is running >> ', Mazda.isRunning); // true
 //* THIS = Child (current) Class
 //* SUPER = Parent Class
 
+/*
 class Vehicle {
     vehicleActive = false
 
-    constructor(vehicleType) {
+    constructor(vehicleType = true) {
         this.vehicleActive = vehicleType
     }
 
@@ -108,12 +109,81 @@ class Car extends Vehicle {
         this.isRunning = true
     }
 }
-
+*/
+/*
 const Mercedes = new Car('Mercedes', 'M300', 1995)
 console.log('all >> ',Mercedes);
 console.log(Mercedes.vehicleActive);
 Mercedes.sayHello()
-
+*/
+/*
 class Accessory extends Car {
-    
+    constructor(accessoryName, brand, model, year, vehicleType = true) {
+        super(brand, model, year, vehicleType)
+        this.accessoryName = accessoryName
+    }
 }
+
+const fordClimate = new Accessory('Bosh Climate', 'Ford', 'Mustang', 1993, false)
+//console.log('all >>', fordClimate);
+*/
+
+/* --------------------------------------------- */
+//? Polymorphism: Miras aldığımız sınıfın özellik/methodlarını yeniden yazabilme.
+//? Override: Üst metodla aynı isim ve yapıda yeni bir metod yazma. (ezme / iptal etme / önceliğini alma)
+//? Overload: Üst metodla aynı isimde ama farklı yapıda (farklı adet/tip) yeni method oluşturma. (aynı anda ikisi de aktif) (JS desteklemez)
+
+/*class Vehicle {
+    vehicleActive = false
+
+    constructor(vehicleType) {
+        this.vehicleActive = vehicleType
+    }
+
+    getDetails() {
+        console.log('Vehicle/getDetails started.')
+        return this
+    }
+
+    getType(vehicleType) {
+        //console.log('Vehicle type: ' + this.vehicleType)
+        console.log(`Vehicle type: ${this.vehicleType}`)
+    }
+}
+
+class Car extends Vehicle {
+    isRunning = false
+    
+    constructor(brand, model, year, vehicleType = true) {
+        super(vehicleType)
+        this.brand = brand
+        this.model = model
+        this.year = year
+    }
+
+    runEngine() {
+        this.isRunning = true
+        console.log('Engine Started')
+        return this.isRunning
+    }
+
+    getDetails() {
+        console.log('Car/getDetails started');
+    }
+
+    getType(vehicleType, brand){
+        console.log(`Vehicle/Brand type ${vehicleType} ${brand}`);
+    }
+}
+
+const Mercedes = new Car('Mercedes', 'M300', 2010, 'Truck')
+
+//console.log(Mercedes);
+
+console.log(Mercedes.vehicleActive);
+
+Mercedes.getDetails()
+
+Mercedes.getType('Car')
+Mercedes.getType('Car', 'Ford')
+*/
