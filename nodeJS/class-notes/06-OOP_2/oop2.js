3f22600d2491cbd0c9288949323aea7d61936f3f
sabanferik <sbnftmaf@gmail.com>
2024-05-29 09:31:08 +0200

    "Added class definitions and instances to oop2.js file"

--- nodeJS/class-notes/06-OOP_2/oop2.js
+++ nodeJS/class-notes/06-OOP_2/oop2.js
@@ -7,7 +7,7 @@
 
 /* ---------------------------------------------
     CLASSES
---------------------------------------------- *
+--------------------------------------------- */
 //? OOP: Object Oriented Programming
 //? DRY: Don't Repeat Yourself
 //? BLUEPRINT: Taslak (Mimarların kullandığı mavi şablon kağıdı)
@@ -18,3 +18,32 @@
 
 // Class Expression:
 // const PascalNamedClass = class { ... }
+
+const Car = class {
+    propertyName = 'value' // attribute, field
+    constructor (parametre1, parametre2 = 'default-value'){
+        this.parametre1 = parametre1
+        this.parametre2 = parametre2
+    }
+    methodName1() {
+        return this
+    }
+}
+//? INSTANCE: Class'tan üretilen nesnelere denir.
+/*const InstanceName = new Car('test')
+console.log(InstanceName);
+console.log(InstanceName.methodName1());
+*/
+
+//? contructor() method
+
+class Car {
+    isRunning = false
+    constructor(brand, model, year) {
+        this.brand = brand
+        this.model = model
+        this.year = year
+    }
+}
+const Ford = new Car('Ford', 'Mustang', 1990)
+console.log(Ford);
