5488a16e29c75070ea4a3409e91f5f53f1d27410
sabanferik <sbnftmaf@gmail.com>
2024-05-29 09:13:02 +0200

    oop2 folder

---
+++ nodeJS/class-notes/06-OOP_2/oop2.js
@@ -0,0 +1,20 @@
+"use strict"
+/* ---------------------------------------------
+
+    OOP - Object Oriented Programing
+
+--------------------------------------------- */
+
+/* ---------------------------------------------
+    CLASSES
+--------------------------------------------- *
+//? OOP: Object Oriented Programming
+//? DRY: Don't Repeat Yourself
+//? BLUEPRINT: Taslak (Mimarların kullandığı mavi şablon kağıdı)
+//? CLASS: Obje türetmek için kullanılacak şablon.
+
+// Class Declaration: Class Tanımlama
+// class PascalNamedClass { ... }
+
+// Class Expression:
+// const PascalNamedClass = class { ... }
