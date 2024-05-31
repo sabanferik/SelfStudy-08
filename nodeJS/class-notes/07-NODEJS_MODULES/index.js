a14937469519a475bf5fc820e501f69177945df7
sabanferik <sbnftmaf@gmail.com>
2024-05-31 11:46:16 +0200

    "Updated index.js to require and call testFunction from module.js, and modified module.js to export testFunction."

--- nodeJS/class-notes/07-NODEJS_MODULES/index.js
+++ nodeJS/class-notes/07-NODEJS_MODULES/index.js
@@ -2,5 +2,36 @@
 console.log("modeule lesson index");
 
 //require('./modules/module.js')
-require('./modules/module')
+// require('./modules/module')
 //require('./modules/') // default index.js
+
+// //! single funciton
+// const testFunction=require('./modules/module')
+// testFunction()
+
+//!multi funciton
+
+// //? array
+// const [test_A,test_B,test_C]=require('./modules/module') // export sırasına göre aktarır
+// test_A() 
+// test_B() 
+// test_C() 
+
+//? obje
+
+// const {test_A,test_B,test_C}=require('./modules/module')
+// test_A()
+// test_C()
+// test_B()
+
+// const {test_A: t_A,test_B,test_C:t_C,pi}=require('./modules/module')
+// t_A() //test_A()
+// t_C() //test_C()
+// test_B()
+// console.log(pi);
+
+const {testFunction_A:t_A, testFunction_B,testFunction_C,message}=require('./modules/module')
+t_A()
+testFunction_B()
+testFunction_C()
+console.log(message);
--- nodeJS/class-notes/07-NODEJS_MODULES/modules/module.js
+++ nodeJS/class-notes/07-NODEJS_MODULES/modules/module.js
@@ -1,2 +1,70 @@
 'use strict'
 console.log(" module page");
+/*
+//! single function
+
+// const testFunction=function(){
+//         console.log('this is test function');
+// }
+
+// module.exports=testFunction
+
+module.exports=function(){
+    console.log('this is test function');
+}
+*/
+//! multi function
+
+// const testFunction_A=function(){
+//     console.log('this is test function A');
+// }
+// const testFunction_B=function(){
+//     console.log('this is test function B');
+// }
+// const testFunction_C=function(){
+//     console.log('this is test function C');
+// }
+// //? array
+// module.exports=[
+//         testFunction_A,
+//         testFunction_B,
+//         testFunction_C
+// ]
+
+//? obje 
+// module.exports={
+//     test_A:testFunction_A,
+//     test_B: testFunction_B,
+//     test_C:testFunction_C,
+//     pi:3.14
+// }
+
+//? shorthand 1
+    // module.exports.testFunction_A=function(){
+    //     console.log('this is test function A');
+    // }
+    // module.exports.testFunction_B=function(){
+    //     console.log('this is test function B');
+    // }
+    // module.exports.testFunction_C=function(){
+    //     console.log('this is test function C');
+    // }
+    // module.exports.message="hello";
+
+//? shorthand 2
+
+//? shorthand 
+module.exports={
+    testFunction_A:function(){
+        console.log('this is test function A');
+    },
+    testFunction_B:function(){
+        console.log('this is test function B');
+    },
+    testFunction_C:function(){
+        console.log('this is test function C');
+    },
+    message:"hello"
+} 
+
+
