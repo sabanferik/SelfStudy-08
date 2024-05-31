0541476e48ff7ec7b9d78313990d3305d4ad1894
sabanferik <sbnftmaf@gmail.com>
2024-05-31 10:56:03 +0200

    "Updated package.json: added description, new script 'deneme', and express dependency"

---
+++ nodeJS/class-notes/07-NODEJS_MODULES/app.js
@@ -0,0 +1,2 @@
+'use strict'
+console.log("modeule lesson app js");
---
+++ nodeJS/class-notes/07-NODEJS_MODULES/index.js
@@ -0,0 +1,6 @@
+'use strict'
+console.log("modeule lesson index");
+
+//require('./modules/module.js')
+require('./modules/module')
+//require('./modules/') // default index.js
