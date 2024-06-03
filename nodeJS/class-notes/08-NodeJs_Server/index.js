41f07d1752140851aaaff384e1332e13e94deb6d
sabanferik <sbnftmaf@gmail.com>
2024-06-03 10:02:02 +0200

    "Added dotenv configuration, defined PORT and HOST variables, and updated HTTP server creation and listening logic."

--- nodeJS/class-notes/08-NodeJs_Server/index.js
+++ nodeJS/class-notes/08-NodeJs_Server/index.js
@@ -4,17 +4,50 @@
 */
 console.log("hi")
 const http=require('node:http') 
+
+// const dotenv=require(dotenv)
+// dotenv.config()
+const dotenv=require('dotenv').config()
+const PORT=process.env.PORT
+const HOST=process.env.HOST
+
+
 /*
 http.createServer((parametre1,parametre2)=>{
 
     console.log("server runned")
 })
 */
-// 1. param request, 2. param response
-
-// http.createServer((request,response)=>{
-const app=http.createServer((req,res)=>{
+/*
+http.createServer((parametre1,parametre2)=>{
 
     console.log("server runned")
+}).listen(8000)
+*/
+// 1. param request, 2. param response
+
+
+// http.createServer((request,response)=>{
+    /*
+const app=http.createServer((req,res)=>{
+    res.end('server runned')
+
+
+    // res.end('bye') // birden fazla olmaz
+    // console.log(req.method);
+    // console.log("server test")
 })
-app.listen(8000)
+// app.listen(8000)
+// app.listen(PORT,()=>console.log('server runned http://127.0.0.1' ))
+
+app.listen(PORT,()=>console.log(`server runned http://${HOST}`))
+*/
+/* SERVER  */
+
+const app=http.createServer((req,res)=>{
+
+
+    res.end('server runned')
+  
+})
+app.listen(PORT,()=>console.log(`server runned http://${HOST}`))
