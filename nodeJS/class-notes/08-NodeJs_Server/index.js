a52e509dbd95edc60fa0b58309e2f1c06b70ccbe
sabanferik <sbnftmaf@gmail.com>
2024-06-03 11:04:10 +0200

    "Updated index.js to add routing and dotenv configuration, and modified package.json and package-lock.json to include dotenv dependency"

--- nodeJS/class-notes/08-NodeJs_Server/index.js
+++ nodeJS/class-notes/08-NodeJs_Server/index.js
@@ -8,8 +8,11 @@ const http=require('node:http')
 // const dotenv=require(dotenv)
 // dotenv.config()
 const dotenv=require('dotenv').config()
-const PORT=process.env.PORT
-const HOST=process.env.HOST
+
+//
+const PORT=process.env.PORT || 8000
+//http://localhost:8000  // domain
+const HOST=process.env.HOST || '127.0.0.1'
 
 
 /*
@@ -46,8 +49,63 @@ app.listen(PORT,()=>console.log(`server runned http://${HOST}`))
 
 const app=http.createServer((req,res)=>{
 
+    /*
+        "/" home page
+        "/FS"  fullstack page
+        "/DS" data science page
+        "CW/api" api page
+    */
+   /*
+    if(req.url=='/'){
 
-    res.end('server runned')
-  
+        res.end('welcome CW')
+
+    }else if(req.url=='/DS') {
+
+        res.end('welcome DS')
+
+    }else if(req.url=='/FS') {
+
+        res.end('welcome FS')
+
+    }else if(req.url=='/CW/api') {
+
+        res.end('welcome api page')
+
+    }else
+        res.end('<h1> no page </h1> ')
+  */
+    // with statuscode
+        // if(req.url=='/' && req.method=='GET'){
+        if(req.url=='/' ){
+            if(req.method=='GET'){
+                res.statusCode=200
+                res.end('welcome CW')
+            }else{
+                res.statusCode=403  // default 200 OK
+                res.end('you can not use this method ')
+            }
+    
+        }else if(req.url=='/DS') {
+    
+            res.end('welcome DS')
+    
+        }else if(req.url=='/FS') {
+    
+            res.end('welcome FS')
+    
+        }else if(req.url=='/CW/api') {
+    
+            res.end('welcome api page')
+    
+        }else{
+            res.statusCode=404
+            res.statusMessage="aradiginiz sayfa yok!"
+            // res.mymessage
+            res.end('<h1> no page </h1> ')
+        }
+
+            
 })
-app.listen(PORT,()=>console.log(`server runned http://${HOST}`))
+
+app.listen(PORT,()=>console.log(`server runned http://${HOST}:${PORT}`))
