acecded769949ba80f8d60d4281057e5d3fcadff
sabanferik <sbnftmaf@gmail.com>
2024-06-04 10:02:48 +0200

    "Added Express.js installation instructions and implemented various routing examples"

--- nodeJS/class-notes/09_Express_Routing_Teach/index.js
+++ nodeJS/class-notes/09_Express_Routing_Teach/index.js
@@ -1,3 +1,6 @@
+// kurulum
+// https://expressjs.com/en/starter/installing.html
+// kill $(lsof -t -i:8000) portu öldürmek
 
 'use strict'
 
@@ -13,10 +16,31 @@ const HOST=process.env.HOST || '127.0.0.1'
 // })
 
 // app.get('/', (req, res) => {
-//     // res.write('Hello World')
+//     // res.write('Hello World)
 //     // res.end()
 //     res.send('Hello World!')
 // })
+
+// app.all('/', (req, res, next) => {  // tüm istekler
+//     console.log('req >> ', req);
+//     next();
+// })
+
+// app.get('/', (req, res) => {res.send('GET method called')}) // listeleme ve/veya bilgi almak için
+// app.post('/', (req, res) => {res.send('POST method called')}) // yeni bir kullanıcı veya bilgi kaydedecekseniz
+// app.put('/', (req, res) => {res.send('PUT method called')}) // bilgi güncellemesi yapmak için
+// app.patch('/', (req, res) => {res.send('PATCH method called')}) // bilgi güncellemesi yapmak için
+// app.delete('/', (req, res) => {res.send('DELETE method called')}) // silme işlemi
+
+
+app.get('/elbise/kadin', (req, res) => {res.send({message: 'GET method called'})}) 
+    // http://127.0.0.1:8000/elbise/kadin
+
+// app.get('/elbise', (req, res) => {res.send({message: 'GET method called (elbise)'})}) 
+// app.get('/elbiseler', (req, res) => {res.send({message: 'GET method called (elbiseler)'})}) 
+// yerine (ler)? 
+app.get('/elbise(ler)?', (req, res) => {res.send({message: 'GET method called (elbise(ler))'})}) 
+
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port http://${HOST}:${PORT}`)
