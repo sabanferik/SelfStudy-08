80adea372ce728c30fceba4b3be140c309e006c7
sabanferik <sbnftmaf@gmail.com>
2024-06-04 09:54:06 +0200

    "Updated index.js and package.json files, adding and modifying dependencies and code blocks."

--- nodeJS/class-notes/09_Express_Routing_Teach/index.js
+++ nodeJS/class-notes/09_Express_Routing_Teach/index.js
@@ -8,9 +8,15 @@ require('dotenv').config()
 const PORT=process.env.PORT || 8000
 const HOST=process.env.HOST || '127.0.0.1'
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+// app.get('/', (req, res) => {
+//     // res.write('Hello World')
+//     // res.end()
+//     res.send('Hello World!')
+// })
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port http://${HOST}:${PORT}`)
