84f04d0d73a13fac038e2c475dd5e922ff5bc689
sabanferik <sbnftmaf@gmail.com>
2024-06-05 11:31:46 +0200

    "Updated middlewares.js: reformatted comments, added new middleware examples, and modified existing code"

--- nodeJS/class-notes/10_Routers_Middlewares/middlewares.js
+++ nodeJS/class-notes/10_Routers_Middlewares/middlewares.js
@@ -1,6 +1,6 @@
 'use strict'
 /* -------------------------------------------------------
-    EXPRESSJS - MIDDLEWARES
+    EXPRESS JS - MIDDLEWARES
 ------------------------------------------------------- */
 
 const express = require('express')
@@ -16,9 +16,81 @@ const HOST = process.env.HOST || '127.0.0.1'
 
 // Middleware:
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+// app.get('/', (req, res, next) => {
+//     console.log('Middleware');
+//     next()
+// })
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+/* ------------------------------------------------------- */
+// http://127.0.0.1:8000?username=clarusway
+// app.get('/', (req, res, next) => { // middleware
+//     console.log(req.query?.username);
+//     if (req.query?.username === 'clarusway'){
+//         next()
+//     } else {
+//         res.send('username is wrong!')
+//     }
+// })
+
+// app.get('/', (req, res) => { // normal router
+//     res.send('welcome clarusway')
+// })
+/* ------------------------------------------------------- */
+
+// app.get('/', (req, res, next) => {
+//     console.log('middleware-1 started.');
+//     next()
+// })
+// app.get('/', (req, res, next) => {
+//     console.log('middleware-2 started.');
+//     next()
+// })
+// app.get('/', (req, res, next) => {
+//     console.log('middleware-3 started.');
+//     next()
+// })
+// app.get('/', (req, res, next) => {
+//     console.log('middleware-4 started.');
+//     next()
+// })
+// app.get('/', (req, res, next) => {
+//     console.log('middleware-5 started.');
+//     next()
+// })
+
+// app.get('/', (req, res) => {
+//     res.send('welcome clarusway')
+// })
+/* ------------------------------------------------------- */
+//? Function
+
+// const middlewareFunction = (req, res, next) => {
+//     console.log('middleware function called');
+//     next()
+// }
+
+// app.get('/', middlewareFunction, (req, res) => {
+//     res.send('welcome clarusway')
+// })
+
+const middlewareFunctionOne = (req, res, next) => {
+    console.log('middleware one function called');
+    next()
+}
+
+const middlewareFunctionTwo = (req, res, next) => {
+    console.log('middleware two function called');
+    next()
+}
+
+app.get('/', middlewareFunctionOne, middlewareFunctionTwo, (req, res) => {
+    res.send('welcome clarusway')
 })
+/* ------------------------------------------------------- */
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port http://${HOST}:${PORT}`)
