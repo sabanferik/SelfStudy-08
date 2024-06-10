fbff3d28cda4271fa8d465c13a3f4274013bedd9
sabanferik <sbnftmaf@gmail.com>
2024-06-10 09:46:49 +0200

    "Updated index.js to include Express app setup, error handling, and JSON parsing; added express-async-error dependency; modified package.json and package-lock.json files accordingly."

--- nodeJS/class-notes/12-TODO__1/index.js
+++ nodeJS/class-notes/12-TODO__1/index.js
@@ -1,14 +1,39 @@
 'use strict'
 
-require('dotenv').config()
-
-
-const PORT=process.env?.PORT ||  8000
-const HOST=process.env?.HOST || '127.0.0.1'
+//npm i express dotenv
+//npm i express-async-error // async func. error control
 
 const express = require('express')
 const app = express()
 
+require('dotenv').config()
+const PORT=process.env?.PORT ||  8000
+const HOST=process.env?.HOST || '127.0.0.1'
+
+app.all('/',(req, res)=>{
+    res.send('TODO APP')
+})
+
+/*
+app.use('/todo',(req, res)=>{ // TODO + ALL url
+    res.send('TODO APP')
+})
+*/
+// json to obj  and obj to json 
+app.use(express.json())
+
+
+// error control
+const errorHandler = (err, req, res, next) => {
+    const errorStatusCode = res?.errorStatusCode || 500
+    res.status(errorStatusCode).send({
+        error: true,
+        status: false,
+        message: err.message,
+        // cause: err.cause,
+        // stack: err.stack
+    })
+}
 
 app.listen(PORT,()=>console.log(`server runned http://${HOST}:${PORT}`))
 
