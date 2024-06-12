5630dc6847f37f7405833471f0faecaa25bbe8d2
sabanferik <sbnftmaf@gmail.com>
2024-06-12 11:02:40 +0200

    "Updated contactController.js and errorHandler.js files"

--- nodeJS/workshop/recap2/src/controllers/contactController.js
+++ nodeJS/workshop/recap2/src/controllers/contactController.js
@@ -1,20 +1,45 @@
 "use strict";
-const Contact = require("../models/contactModel")
+const Contact = require("../models/contactModel");
+const { sequelize } = require("../configs/db");
 module.exports = {
   list: async (req, res) => {
-    const data = await Contact.findAndCountAll()
-    console.log(data)
+    // const data = await sequelize.query("SELECT * FROM contacts") //* klasik sql sorguların yerine ORM kullanıyoruz.
+    // const data = await Contact.findAndCountAll()
+    // const data = await Contact.findAll()
+    const data = await Contact.findAll({
+      where: {
+        isActive: true,
+      },
+    });
+    console.log(data);
     res.status(200).send({
-      error:false,
-      contacts: data
-    })
+      error: false,
+      contacts: datas,
+    });
   },
   create: async (req, res) => {
-    const data = await Contact.create(req.body)
+    // const { firstName, lastName, email, phone, address, isActive } = req.body;
+    // const data = await sequelize.query(
+    //   "INSERT INTO contacts (firstName,lastName,email,phone,address,createdAt,updatedAt,isActive) VALUES (?,?,?,?,?,?,?,?)",
+    //   {
+    //     replacements: [
+    //       firstName,
+    //       lastName,
+    //       email,
+    //       phone,
+    //       address,
+    //       new Date(),
+    //       new Date(),
+    //       isActive || true,
+    //     ],
+    //   }
+    // );//* Klasik sql sorgusu
+    const data = await Contact.create(req.body); //! ORM
+
     res.status(201).send({
-      error:false,
-      contact:data
-    })
+      error: false,
+      contact: data,
+    });
   },
   get: async (req, res) => {},
   update: async (req, res) => {},
