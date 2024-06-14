86d4013ed403b8d16e3e2a8ea49cd9d3e0e03781
sabanferik <sbnftmaf@gmail.com>
2024-06-14 11:06:36 +0200

    "Added blog controller and route files with CRUD operations"

--- nodeJS/class-notes/15-BlogApi-Mongoose/src/controllers/blogController.js
+++ nodeJS/class-notes/15-BlogApi-Mongoose/src/controllers/blogController.js
@@ -1,1 +1,21 @@
 //*Blog Controller
+const { BlogPost } = require("../models/blogModel");
+
+module.exports.BlogPostController = {
+  list: async (req, res) => {
+    const data = await BlogPost.find();
+
+    res.status(200).send({
+      error: false,
+      blogs: data,
+    });
+  },
+  create: async (req, res) => {
+    const data = await BlogPost.create(req.body);
+
+    res.status(201).send({
+      error: false,
+      blog: data,
+    });
+  },
+};
--- nodeJS/class-notes/15-BlogApi-Mongoose/src/routes/blogRoute.js
+++ nodeJS/class-notes/15-BlogApi-Mongoose/src/routes/blogRoute.js
@@ -1,1 +1,10 @@
 //* Blog Route
+
+const router = require("express").Router();
+
+const { BlogPostController } = require("../controllers/blogController");
+
+router
+  .route("/post")
+  .get(BlogPostController.list)
+  .post(BlogPostController.create);
