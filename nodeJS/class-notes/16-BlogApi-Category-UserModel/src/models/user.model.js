06e308bce4224f0b9ce9ea883458efa697867e4e
sabanferik <sbnftmaf@gmail.com>
2024-06-24 12:13:02 +0200

    "Added password encryption functionality to user model"

--- nodeJS/class-notes/16-BlogApi-Category-UserModel/src/models/user.model.js
+++ nodeJS/class-notes/16-BlogApi-Category-UserModel/src/models/user.model.js
@@ -1,5 +1,24 @@
 const mongoose = require("mongoose");
 
+// const crypto = require('node:crypto')
+
+// const keyCode = process.env.SECRET_KEY
+// const loopCount = 10000
+// const charCount = 32 // write 32 for 64 
+// const encType = 'sha512'
+
+// const passwordEncrpyt = (password) => {
+//   // const newPass = crypto.pbkdf2Sync(password,keyCode,loopCount,charCount,encType).toString("hex");
+//   // console.log(newPass)
+//   return crypto
+//     .pbkdf2Sync(password, keyCode, loopCount, charCount, encType)
+//     .toString("hex");
+// }
+
+// passwordEncrpyt("123456")
+// passwordEncrpyt("1234576")
+const passwordEncrpyt = require("../helpers/passwordEncrpyt")
+
 const UserSchema = new mongoose.Schema(
   {
     email: {
@@ -23,6 +42,8 @@ const UserSchema = new mongoose.Schema(
       type: String,
       trim: true,
       required: true,
+      // set: (password) => "anthony",
+      set: (password) => passwordEncrpyt(password),
     },
     firstName: String,
     lastName: String,
