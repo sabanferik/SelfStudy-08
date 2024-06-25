9680cba27b43d9a252c20af44ce268a3a0a2ff3b
sabanferik <sbnftmaf@gmail.com>
2024-06-25 10:51:33 +0200

    "Updated index.js and user.controller.js files with minor code refactoring and added session cookie functionality"

--- nodeJS/class-notes/17-BlogApi-SessionCookies/index.js
+++ nodeJS/class-notes/17-BlogApi-SessionCookies/index.js
@@ -7,32 +7,39 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-require("./src/configs/dbConnection")
+require("./src/configs/dbConnection");
 /* ------------------------------------------------------- */
 // SessionCookies:
 // http://expressjs.com/en/resources/middleware/cookie-session.html
 // https://www.npmjs.com/package/cookie-session
 //* $ npm i cookie-session
 
-const session = require("cookie-session")
+const session = require("cookie-session");
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.SECRET_KEY,
     // maxAge: 1000 * 60 * 60 * 24 * 3 // miliseconds // 3 days
-}))
-
+  })
+);
 
 // HomePage:
-app.all('/', (req, res) => {
-    res.send("<h1 style='text-align:center;margin-top:150px'>WELCOME TO BLOG API</h1>");
-})
+// app.all('/', (req, res) => {
+//     res.send("<h1 style='text-align:center;margin-top:150px'>WELCOME TO BLOG API</h1>");
+// })
+app.all("/", (req, res) => {
+  res.send({
+    message: "Welcome to BlogApi",
+    session: req.session,
+  });
+});
 
-app.use("/blog", require("./src/routes/blogRoute"))
-app.use("/user",require("./src/routes/user.route"))
+app.use("/blog", require("./src/routes/blogRoute"));
+app.use("/user", require("./src/routes/user.route"));
 
 // errorHandler:
-app.use(require('./src/middlewares/errorHandler'))
+app.use(require("./src/middlewares/errorHandler"));
 
-app.listen(PORT, () => console.log('Running: http://127.0.0.1:' + PORT))
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
 
 // require("./src/configs/sync")()
--- nodeJS/class-notes/17-BlogApi-SessionCookies/src/controllers/user.controller.js
+++ nodeJS/class-notes/17-BlogApi-SessionCookies/src/controllers/user.controller.js
@@ -47,7 +47,7 @@ module.exports = {
     }
   },
   login: async (req, res) => {
-    const {email,password} = req.body;
+    const {email,password,remindMe} = req.body;
 
     if(email && password) {
       // const user = await User.findOne({email:email})
@@ -56,11 +56,27 @@ module.exports = {
         // console.log("db: ", user.password)
         // console.log("user: ",passwordEncrpyt(password));
         if(user.password == passwordEncrpyt(password)){
-          res.status(200).send({
-            error:false,
-            message:"Login Ok!",
-            user
-          })
+
+          /*! Session */
+          //* oturum süresince erişim sağlanır
+          // req.session = {
+          //   email: user.email,
+          //   password:user.password
+          // }
+          req.session.email = user.email
+          req.session.password = user.password
+          req.session.id = user._id
+
+          if(remindMe) {
+            req.session.remindMe = remindMe
+            //* sessionu cookieye çeviriyoruz. Verdiğimiz süre kadar erişim sağlanır
+            req.sessionOptions.maxAge = 1000 * 60 * 60 * 24 * 3;
+          }
+            res.status(200).send({
+              error: false,
+              message: "Login Ok!",
+              user,
+            });
         }else {
           res.errorStatusCode = 401;
           throw new Error("Login parameters not true!");
