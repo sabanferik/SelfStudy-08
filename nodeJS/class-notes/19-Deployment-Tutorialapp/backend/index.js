0ea06cae8b8a89f9c78da8ced7546d3cc1bca4dc
sabanferik <sbnftmaf@gmail.com>
2024-06-28 09:45:20 +0200

    "Added dotenv config, mongoose connection, and error handler middleware to backend/index.js; updated mongooseConnection.js to throw CustomError instead of Error"

--- nodeJS/class-notes/19-Deployment-Tutorialapp/backend/index.js
+++ nodeJS/class-notes/19-Deployment-Tutorialapp/backend/index.js
@@ -1,6 +1,8 @@
 "use strict";
 require("express-async-errors");
 const express = require("express");
+require("dotenv").config()
+const { mongooseConnection } = require("./startup/mongooseConnection");
 const PORT = process.env?.PORT || 8000;
 
 const app = express();
@@ -10,4 +12,6 @@ app.all("/", (req, res) => {
   res.send("Hello TutorialApp");
 });
 
+app.use(require("./middlewares/errorHandler"))
+mongooseConnection()
 app.listen(PORT, () => console.log("Listening http://127.0.0.1:" + PORT));
