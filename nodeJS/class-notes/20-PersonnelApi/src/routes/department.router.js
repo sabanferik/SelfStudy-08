319c9bcafd14142f1e78d95d25e30dc8fd2be45a
sabanferik <sbnftmaf@gmail.com>
2024-07-01 12:19:30 +0200

    department put

--- nodeJS/class-notes/20-PersonnelApi/src/routes/department.router.js
+++ nodeJS/class-notes/20-PersonnelApi/src/routes/department.router.js
@@ -11,12 +11,15 @@ const idValidation = require("../middlewares/idValidation");
 //* URL : /departments
 
 router.route("/").get(department.list).post(department.create);
-router.route("/:id/personnels").get(department.personnels)
+
+router.route("/:id/personnels").get(department.personnels);//* departmana göre personel listeleme
+
 router
   .route("/:id")
   .all(idValidation)
   .get(department.read)
   .put(department.update)
+  .patch(department.update)
   .delete(department.delete);
 
 /* ------------------------------------------------------- */
