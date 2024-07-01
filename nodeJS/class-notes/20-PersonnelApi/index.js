01be7855fdac54b92acb54bddc6a17dc10743a52
sabanferik <sbnftmaf@gmail.com>
2024-07-01 09:26:19 +0200

    "Added required modules, configurations, middlewares, and routes to index.js"

--- nodeJS/class-notes/20-PersonnelApi/index.js
+++ nodeJS/class-notes/20-PersonnelApi/index.js
@@ -10,11 +10,42 @@ const express = require('express')
 const app = express()
 
 /* ------------------------------------------------------- */
+// Required Modules
+
+//* envVariables to process.env
+require('dotenv').config()
+const PORT = process.env.PORT || 8000;
+
+//? asyncErrors to errorHandler
+require("express-async-errors")
+
+/* -------------------------------------------------------------------------- */
+/*                               Configurations                               */
+/* -------------------------------------------------------------------------- */
+
+//! database connection
+const {dbConnection} = require("./src/configs/dbConnection")
+dbConnection()
 
 
+/* -------------------------------------------------------------------------- */
+/*                                 MiddleWares                                */
+/* -------------------------------------------------------------------------- */
 
-// continue from here...
+//* accept json
+app.use(express.json())
 
+
+//*Filter,Search,Sort,Pagination(res.getModelList)
+require("./src/middlewares/findSearchSortPagi")
+
+/* -------------------------------------------------------------------------- */
+/*                                   Routes                                   */
+/* -------------------------------------------------------------------------- */
+
+app.all("/",(req,res)=> {
+    res.send("Welcome to the Personnel API")
+})
 
 
 /* ------------------------------------------------------- */
