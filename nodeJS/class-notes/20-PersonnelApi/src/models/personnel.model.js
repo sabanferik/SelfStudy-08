4b83014439858d3072cc72e8d5196a47da531d27
sabanferik <sbnftmaf@gmail.com>
2024-07-01 10:44:20 +0200

    "Added PersonnelSchema to personnel.model.js, including fields for department, username, password, personal details, and role information, with validation and encryption for password."

--- nodeJS/class-notes/20-PersonnelApi/src/models/personnel.model.js
+++ nodeJS/class-notes/20-PersonnelApi/src/models/personnel.model.js
@@ -2,5 +2,89 @@
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require('../configs/dbConnection');
+const passwordEncrypt = require('../helpers/passwordEncrypt');
 /* ------------------------------------------------------- */
+
+const PersonnelSchema = new mongoose.Schema(
+  {
+    departmentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Department",
+      required: true,
+    },
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      set: (password) => passwordEncrypt(password),
+    },
+    firstName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    phone: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      validate: [
+        (email) => email.includes("@") && email.split("@")[1].includes("."),
+        "Email is invalid!",
+      ],
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    salary: {
+      type: number,
+      default: 0,
+    },
+    description: {
+      type: String,
+      trim: true,
+      default: null,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isLead: {
+      type: Boolean,
+      default: false,
+    },
+    startedAt: {
+        type: Date,
+        default: Date.now(),
+    }
+  },
+  {
+    collection: "personnel",
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Personnel",PersonnelSchema)
