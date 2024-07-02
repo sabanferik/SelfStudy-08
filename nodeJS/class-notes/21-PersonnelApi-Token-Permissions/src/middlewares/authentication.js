7e8acb342abd0131f51f6ba08e1d9a55eeaea651
sabanferik <sbnftmaf@gmail.com>
2024-07-02 11:24:59 +0200

    "Updated auth controller, added logout functionality, and modified authentication middleware and auth router"

--- nodeJS/class-notes/21-PersonnelApi-Token-Permissions/src/middlewares/authentication.js
+++ nodeJS/class-notes/21-PersonnelApi-Token-Permissions/src/middlewares/authentication.js
@@ -15,13 +15,13 @@ module.exports = async (req, res, next) => {
 //   console.log(req.headers);
 
   const auth = req.headers?.authorization || null;
-  console.log(auth);
+//   console.log(auth);
   const tokenKey = auth ? auth.split(" ") : null
-  console.log(tokenKey);
+//   console.log(tokenKey);
 
   if(tokenKey && tokenKey[0] == 'Token'){
     const tokenData = await Token.findOne({ token: tokenKey[1] }).populate("userId");
-    console.log(tokenData);
+    // console.log(tokenData);
     if(tokenData) req.user = tokenData.userId
   }
 
