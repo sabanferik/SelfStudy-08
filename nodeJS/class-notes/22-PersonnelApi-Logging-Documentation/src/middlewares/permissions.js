d366622f12dc8ea28742de9adecbe7a79d12ef89
sabanferik <sbnftmaf@gmail.com>
2024-07-04 09:30:19 +0200

    "Updated permissions middleware and routes for department and token APIs"

--- nodeJS/class-notes/22-PersonnelApi-Logging-Documentation/src/middlewares/permissions.js
+++ nodeJS/class-notes/22-PersonnelApi-Logging-Documentation/src/middlewares/permissions.js
@@ -24,11 +24,26 @@ module.exports = {
     }
   },
   isAdminOrOwn: (req, res, next) => {
-    if (req.user && req.user.isActive && (req.user.isAdmin || req.user._id == req.params?.id )) {
+    if (
+      req.user &&
+      req.user.isActive &&
+      (req.user.isAdmin || req.user._id == req.params?.id)
+    ) {
       next();
     } else {
       res.errorStatusCode = 403;
-      throw new Error("NoPermission : You must login and to be Admin or Owner!");
+      throw new Error(
+        "NoPermission : You must login and to be Admin or Owner!"
+      );
+    }
+  },
+  isAdminOrLead: (req, res, next) => {
+    //* req.params.id => departmenta ait id bilgisini veriyor
+    if (req.user && req.user.isActive && (req.user.isAdmin || (req.user.isLead && req.user.departmentId == req.params?.id))) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission : You must login and to be Admin or Department Lead!");
     }
   },
 };
