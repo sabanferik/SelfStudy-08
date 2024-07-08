55167a5db6ee7419ee51d4097fa859e700ada675
sabanferik <sbnftmaf@gmail.com>
2024-07-08 11:03:15 +0200

    "Added auth controller with login functionality and Token model integration"

--- nodeJS/class-notes/24-PizzaApi/src/controllers/auth.js
+++ nodeJS/class-notes/24-PizzaApi/src/controllers/auth.js
@@ -2,3 +2,48 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+
+const User = require("../models/user")
+const Token = require("../models/token");
+const { CustomError } = require("../errors/customError");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
+
+module.exports = {
+    login: async (req,res)=>{
+        const {username,email,password} = req.body;
+
+        if(password && (username || email)) {
+            // const user = await User.findOne({username,email}) //* and operatörü
+            const user = await User.findOne({$or:[{username},{email}]})
+            if(user && user.password == passwordEncrypt(password)){
+                if(user.isActive){
+                    let tokenData = await Token.findOne({userId:user._id})
+                    if(!tokenData){
+                        tokenData = await Token.create({
+                          userId: user._id,
+                          token:passwordEncrypt(user._id + Date.now())
+                        });
+                    }
+
+                    res.status(200).send({
+                        error:false,
+                        token:tokenData.token,
+                        user
+                    })
+                }else{
+                    throw new CustomError("This account is inactive!",401)
+                }
+            }else {
+                throw new Error("Wrong username/email or password!",401)
+            }
+
+        }else {
+            throw new CustomError("Please enter username/email and password!", 401)
+        }
+
+    },
+    logout: async (req,res)=>{
+
+    },
+}
