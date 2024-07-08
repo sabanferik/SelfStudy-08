dce300751a6c183f661143bf39b57503a723e99f
sabanferik <sbnftmaf@gmail.com>
2024-07-08 11:54:33 +0200

    "Added Topping API endpoints and model, including CRUD operations and middleware for ID validation"

--- nodeJS/class-notes/24-PizzaApi/src/models/topping.js
+++ nodeJS/class-notes/24-PizzaApi/src/models/topping.js
@@ -4,3 +4,17 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+
+const ToppingSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim:true,
+        required: true,
+        unique:true
+    }
+},{
+    collection: 'toppings',
+    timestamps:true
+})
+
+module.exports =mongoose.model("Topping", ToppingSchema)
