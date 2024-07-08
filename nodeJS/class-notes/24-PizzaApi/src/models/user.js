289e18b8ab88cd3296b6353db9b481eefa27e6ee
sabanferik <sbnftmaf@gmail.com>
2024-07-08 09:38:48 +0200

    "Added user controller with CRUD operations and updated user model with schema definition and password encryption"

--- nodeJS/class-notes/24-PizzaApi/src/models/user.js
+++ nodeJS/class-notes/24-PizzaApi/src/models/user.js
@@ -2,5 +2,44 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require('../configs/dbConnection');
+const passwordEncrypt = require('../helpers/passwordEncrypt');
 /* ------------------------------------------------------- */
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      //   set: passwordEncrypt,
+      set: (password) => passwordEncrypt(password),
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    collection: "users",
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User",UserSchema)
