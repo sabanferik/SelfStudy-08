b2e696edb20227e74504007e778e074cd8762b68
sabanferik <sbnftmaf@gmail.com>
2024-07-09 08:43:08 +0200

    "Added Swagger annotations to controllers for API documentation"

--- nodeJS/class-notes/24-PizzaApi/src/controllers/auth.js
+++ nodeJS/class-notes/24-PizzaApi/src/controllers/auth.js
@@ -10,6 +10,19 @@ const passwordEncrypt = require("../helpers/passwordEncrypt");
 
 module.exports = {
   login: async (req, res) => {
+    /*
+            #swagger.tags = ["Authentication"]
+            #swagger.summary = "Login"
+            #swagger.description = 'Login with username (or email) and password for get simpleToken and JWT'
+            #swagger.parameters["body"] = {
+                in: "body",
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "aA?123456",
+                }
+            }
+        */
     const { username, email, password } = req.body;
 
     if (password && (username || email)) {
@@ -41,6 +54,11 @@ module.exports = {
     }
   },
   logout: async (req, res) => {
+    /*
+            #swagger.tags = ["Authentication"]
+            #swagger.summary = "simpleToken: Logout"
+            #swagger.description = 'Delete token key.'
+        */
     const auth = req.headers?.authorization;
     const tokenKey = auth ? auth.split(" ") : null;
 
