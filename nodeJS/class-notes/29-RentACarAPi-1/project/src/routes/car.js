1a2822b6b08ab816d04bde2229533f0d77faa6e2
sabanferik <sbnftmaf@gmail.com>
2024-07-18 11:04:57 +0200

    "Updated car controller, added permissions middleware, and configured routes for car API"

--- nodeJS/class-notes/29-RentACarAPi-1/project/src/routes/car.js
+++ nodeJS/class-notes/29-RentACarAPi-1/project/src/routes/car.js
@@ -6,8 +6,23 @@ const router = require('express').Router()
 /* ------------------------------------------------------- */
 // routes/car:
 
-const car = require('../controllers.car')
+const car = require('../controllers/car')
 
-const permissions = require('../middleware.permissions')
+const permissions = require('../middlewares/permissions')
+
+// URL: /cars
+
+router.route('/')
+    .get(car.list)
+    .post(permissions.isStaff, car.create)
+
+router.route('/:id')
+    .get(car.read)
+    .put(permissions.isStaff, car.update)
+    .patch(permissions.isStaff, car.update)
+    .delete(permissions.isAdmin, car.delete)
+
+
+
 
 module.exports = router
