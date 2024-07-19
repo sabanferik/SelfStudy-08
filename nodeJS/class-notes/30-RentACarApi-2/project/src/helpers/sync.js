99413e396cfb79e39b5ea87803b84887a425c07a
sabanferik <sbnftmaf@gmail.com>
2024-07-19 09:21:35 +0200

    "Updated sync.js to add admin, staff, and user creation, and commented out car creation"

--- nodeJS/class-notes/30-RentACarApi-2/project/src/helpers/sync.js
+++ nodeJS/class-notes/30-RentACarApi-2/project/src/helpers/sync.js
@@ -4,7 +4,7 @@
 ------------------------------------------------------- */
 // sync():
 
-module.exports = async function() {
+module.exports = async function () {
 
     return null;
 
@@ -14,4 +14,74 @@ module.exports = async function() {
     console.log('- Database and all data DELETED!')
     /* REMOVE DATABASE */
 
+    const User = require('../models/user')
+    const Car = require('../models/car')
+
+    await User.create({
+        username: "admin",
+        password: "aA?123456",
+        email: "admin@site.com",
+        firstName: "admin",
+        lastName: "admin",
+        isActive: true,
+        isStaff: true,
+        isAdmin: true
+    })
+    console.log('- Admin user created.')
+
+    await User.create({
+        username: "staff",
+        password: "aA?123456",
+        email: "staff@site.com",
+        firstName: "staff",
+        lastName: "staff",
+        isActive: true,
+        isStaff: true,
+        isAdmin: false
+    })
+    console.log('- Staff user created.')
+
+    await User.create({
+        username: "user",
+        password: "aA?123456",
+        email: "user@site.com",
+        firstName: "user",
+        lastName: "user",
+        isActive: true,
+        isStaff: false,
+        isAdmin: false
+    })
+    console.log('- User created.')
+
+    /*
+    await Car.create({
+        plateNumber: "34ABC1234",
+        brand: "Ford",
+        model: "Focus",
+        year: 2020,
+        isAutomatic: true,
+        pricePerDay: 149.99
+    })
+    console.log('- Ford Car created.')
+
+    await Car.create({
+        plateNumber: "34ABC1235",
+        brand: "Renault",
+        model: "Megane",
+        year: 2022,
+        isAutomatic: true,
+        pricePerDay: 209.99
+    })
+    console.log('- Ford Car created.')
+
+    await Car.create({
+        plateNumber: "34ABC1236",
+        brand: "Opel",
+        model: "Astra",
+        year: 2024,
+        isAutomatic: true,
+        pricePerDay: 249.99
+    })
+    console.log('- Ford Car created.')
+    */
 }
