6c4a07f650c0990631a08823e35ebdccccebf4ad
sabanferik <sbnftmaf@gmail.com>
2024-07-24 10:46:26 +0200

    "Added boilerplate code for controllers and routes in StockApi project"

--- nodeJS/class-notes/32-StockApi-2/project/src/controllers/product.js
+++ nodeJS/class-notes/32-StockApi-2/project/src/controllers/product.js
@@ -2,3 +2,24 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+// Product Controller
+
+const Firm = require('../models/product')
+
+module.exports = {
+
+    list: async (req, res) => {
+    },
+
+    create: async (req, res) => {
+    },
+
+    read: async (req, res) => {
+    },
+
+    update: async (req, res) => {
+    },
+
+    delete: async (req, res) => {
+    },
+}
--- nodeJS/class-notes/32-StockApi-2/project/src/controllers/sale.js
+++ nodeJS/class-notes/32-StockApi-2/project/src/controllers/sale.js
@@ -2,3 +2,24 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+// Sale Controller
+
+const Firm = require('../models/sale')
+
+module.exports = {
+
+    list: async (req, res) => {
+    },
+
+    create: async (req, res) => {
+    },
+
+    read: async (req, res) => {
+    },
+
+    update: async (req, res) => {
+    },
+
+    delete: async (req, res) => {
+    },
+}
