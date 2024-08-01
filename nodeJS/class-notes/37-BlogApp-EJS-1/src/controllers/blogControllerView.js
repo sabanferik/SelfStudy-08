ab8c1258410d994d04c8f67b23b131dc49624126
sabanferik <sbnftmaf@gmail.com>
2024-08-01 10:31:50 +0200

    "Updated index.ejs template to dynamically render categories and post content, and added categories to blogControllerView.js"

--- nodeJS/class-notes/37-BlogApp-EJS-1/src/controllers/blogControllerView.js
+++ nodeJS/class-notes/37-BlogApp-EJS-1/src/controllers/blogControllerView.js
@@ -63,6 +63,8 @@ module.exports.BlogPostController = {
       { path: "userId" },
     ]);
 
+    const categories = await BlogCategory.find();
+
     // res.status(200).send({
     //   error: false,
     //   details: await res.getModelListDetails(BlogPost),
@@ -70,6 +72,7 @@ module.exports.BlogPostController = {
     // });
     res.render('index',{
       posts:data,
+      categories,
     })
   },
   create: async (req, res) => {
