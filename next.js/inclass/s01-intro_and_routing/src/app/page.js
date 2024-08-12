b8315c3f133a1a435ca6554162d52a80fbbfcbae
sabanferik <sbnftmaf@gmail.com>
2024-08-12 10:58:49 +0200

    tailwind.config color und andere updated

--- next.js/inclass/s01-intro_and_routing/src/app/page.js
+++ next.js/inclass/s01-intro_and_routing/src/app/page.js
@@ -1,3 +1,5 @@
+//? app folder içindeki page.js "/" route olan home sayfasıdır
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
