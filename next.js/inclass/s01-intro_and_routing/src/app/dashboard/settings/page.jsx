fccad4378000cfd5438d6cbb3ee4957ee4fb7d45
sabanferik <sbnftmaf@gmail.com>
2024-08-12 12:05:26 +0200

    "Added 'use client' directive and useRouter hook to not-found.jsx, and added 'use client' directive to page.js"

---
+++ next.js/inclass/s01-intro_and_routing/src/app/dashboard/settings/page.jsx
@@ -0,0 +1,15 @@
+import GoBack from "@/components/GoBack";
+import React from "react";
+
+const Settings = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center">
+        Hello, this is Settings Page
+      </h1>
+      <GoBack />
+    </div>
+  );
+};
+
+export default Settings;
--- next.js/inclass/s01-intro_and_routing/src/app/page.js
+++ next.js/inclass/s01-intro_and_routing/src/app/page.js
@@ -1,5 +1,7 @@
 //? app folder içindeki page.js "/" route olan home sayfasıdır
 
+//? nextjs'de tüm componentler default olarak server componenttir. client component kullanmak için "use-client" direktifini kullanmamız gerekir
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
