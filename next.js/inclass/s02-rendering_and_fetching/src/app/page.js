3e7174894a3fe1bf4014a821fd338dd95e7cd4a7
sabanferik <sbnftmaf@gmail.com>
2024-08-13 11:00:18 +0200

    "Updated globals.css with new CSS classes and modified page.js to import and use new components"

--- next.js/inclass/s02-rendering_and_fetching/src/app/page.js
+++ next.js/inclass/s02-rendering_and_fetching/src/app/page.js
@@ -1,7 +1,14 @@
+import ClientComponent from "@/components/ClientComponent";
+import ServerComponent from "@/components/ServerComponent";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>s02</h1>
+      <h1 className="text-3xl font-bold">Hello, Dashboard Page</h1>
+      {/* <ServerComponent /> */}
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
     </main>
   );
 }
