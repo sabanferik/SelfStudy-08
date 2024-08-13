560ef0aac6c40b7bcd92f7ebb4f4d167ab25ddcc
sabanferik <sbnftmaf@gmail.com>
2024-08-13 12:06:35 +0200

    "Updated next.config.mjs to allow remote images from reqres.in and modified team/page.jsx to import TeamList component and use it to display users"

--- next.js/inclass/s02-rendering_and_fetching/next.config.mjs
+++ next.js/inclass/s02-rendering_and_fetching/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "reqres.in",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
