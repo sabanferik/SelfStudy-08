a81baf534a8529f88a24f8c440cc0edf9bf3ffee
sabanferik <sbnftmaf@gmail.com>
2024-08-13 09:03:22 +0200

    "Updated UserDetail component to use params, added generateStaticParams function, and modified JSX structure"

--- next.js/inclass/s01-intro_and_routing/src/app/dashboard/users/[userId]/page.jsx
+++ next.js/inclass/s01-intro_and_routing/src/app/dashboard/users/[userId]/page.jsx
@@ -1,10 +1,13 @@
 import GoBack from "@/components/GoBack";
+// import { useParams } from "next/navigation";
 import React from "react";
 
 const UserDetail = ({ params }) => {
+  //* Url parametresini client componentlerde useParams hooku ile, server componentlerde props üzerinden yakalayabiliyoruz.
+  //   const {userId} =  useParams()
   const { userId } = params;
   return (
-    <div>
+    <div className="container mx-auto px-6 py-6">
       <h1 className="text-3xl text-center"> User {userId} </h1>
       <GoBack />
     </div>
@@ -12,3 +15,11 @@ const UserDetail = ({ params }) => {
 };
 
 export default UserDetail;
+
+//* dinamik sayfalar için meta-data oluşturma
+export async function generateStaticParams() {
+  const userArr = [1, 2, 3, 4];
+  return userArr.map((userId) => ({
+    userId: userId.toString(),
+  }));
+}
