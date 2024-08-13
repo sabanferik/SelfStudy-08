f231804ba16d96691ef92702f9119132fe8f175f
sabanferik <sbnftmaf@gmail.com>
2024-08-13 11:48:27 +0200

    "Added async functionality to Team component and created getUsers function in apiFunctions.js"

--- next.js/inclass/s02-rendering_and_fetching/src/app/(public)/team/page.jsx
+++ next.js/inclass/s02-rendering_and_fetching/src/app/(public)/team/page.jsx
@@ -1,6 +1,9 @@
+import { getUsers } from "@/helpers/apiFunctions";
 import React from "react";
 
-const Team = () => {
+const Team = async () => {
+  const users = await getUsers();
+  console.log(users);
   return (
     <div className="text-2xl text-center">
       <h1>Team</h1>
