9e0bc55a03fa401bd90697b63ff8af4f8b319254
sabanferik <sbnftmaf@gmail.com>
2024-08-20 09:48:55 +0200

    "Updated apiFunctions.js: commented out old fetch code and added new fetch code with next.revalidate option"

--- next.js/inclass/s02-rendering_and_fetching/src/helpers/apiFunctions.js
+++ next.js/inclass/s02-rendering_and_fetching/src/helpers/apiFunctions.js
@@ -20,10 +20,10 @@ const URL = `http://localhost:8080/users`;
 
 //* revalidate
 // export const getUsers = async () => {
-//   const res = await fetch(URL);
-//   //    'force-cache' is the default, and can be omitted
-//   //    const res = await fetch(URL, { cache: 'force-cache' });
-//   if (!res.ok) {
+  //   //* Automatically revalidate data after a certain amount of time has passed. This is useful for data that changes infrequently and freshness is not as critical.
+  //   //? To revalidate data at a timed interval, you can use the next.revalidate option of fetch to set the cache lifetime of a resource (in seconds). 
+//   const res = await fetch(URL,{ next: {revalidate: 10}});
+ //   if (!res.ok) {
 //     // This will activate the closest `error.js` Error Boundary
 //     throw new Error("Failed to fetch data");
 //   }
