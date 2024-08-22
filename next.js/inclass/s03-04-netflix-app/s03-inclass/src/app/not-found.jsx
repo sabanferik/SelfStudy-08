e4404d8c12b8f3d06fc9c8d24a0928205a34ad2b
sabanferik <sbnftmaf@gmail.com>
2024-08-22 12:18:15 +0200

    "Removed extra whitespace in NotFound component's button className"

--- next.js/inclass/s03-04-netflix-app/s03-inclass/src/app/not-found.jsx
+++ next.js/inclass/s03-04-netflix-app/s03-inclass/src/app/not-found.jsx
@@ -9,7 +9,7 @@ export default function NotFound() {
     <div className="h-screen flex flex-col justify-center items-center">
       <img src="/images/404-page-not-found.png" />
       <button
-        className="text-gray-100 bg-gray-800 hover:bg-gray-950 font-semibold py-2 px-4 rounded shadow mt-4"
+        className=" text-gray-100 bg-gray-800 hover:bg-gray-950 font-semibold py-2 px-4 rounded shadow mt-4"
         type="button"
         onClick={() => router.back()}
       >
