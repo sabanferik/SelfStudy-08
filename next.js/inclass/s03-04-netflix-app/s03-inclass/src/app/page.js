8617575d6c0e6ac8e89938ffb25e4647729898e8
sabanferik <sbnftmaf@gmail.com>
2024-08-23 12:25:48 +0200

    "Minor styling and formatting adjustments to NotFound and Home components"

--- next.js/inclass/s03-04-netflix-app/s03-inclass/src/app/page.js
+++ next.js/inclass/s03-04-netflix-app/s03-inclass/src/app/page.js
@@ -1,10 +1,10 @@
- import Link from "next/link";
+import Link from "next/link";
 
- export default function Home() {
+export default function Home() {
   return (
     <main className="h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover ">
-       <div className="bg-black w-full h-full bg-opacity-50">
-       <div className="text-white text-center relative top-2/4 m-auto">
+      <div className="bg-black w-full h-full bg-opacity-50">
+        <div className="text-white text-center relative top-2/4 m-auto">
           <h1 className="text-5xl font-[900] text-center mb-3 ">
             Unlimited movies, TV shows, and more
           </h1>
@@ -12,10 +12,10 @@
             Watch anywhere. Cancel anytime.
           </p>
           <Link href="/register">
-          <button className="btn-danger w-[250px]">Get Started</button>
+            <button className="btn-danger w-[250px]">Get Started</button>
           </Link>
-          </div>
-       </div>
+        </div>
+      </div>
     </main>
   );
 }
